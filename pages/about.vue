54a72ac939d0aad6cf1197193ab983735ab85982
pchae <20016424+pchae@users.noreply.github.com>
2022-11-08 03:51:41 -0500

    update layout

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="">
+    <div>
 
         <Head>
             <Title>About Saturdays.io</Title>
@@ -8,17 +8,25 @@
 
         <Nav />
 
-        <section id="bio" class="container mt-6 py-6 px-4">
-            <div class="content is-medium">
-                <h1 class="title has-text-white">We bridge the gap between design and technology</h1>
+        <section class="container mt-6 py-6 px-4">
+            <div class="content my-6 is-medium">
+                <h1 class="is-size-1 has-text-centered has-text-weight-bold has-text-white">
+                    We bridge the gap between design and technology
+                </h1>
             </div>
+        </section>
+
+        <section id="ethics" class="container py-6 px-4">
             <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">We believe that our work and ethics
+                <div class="content is-medium">
+                    <h1 class="is-size-2 has-text-centered text-gradient-blue-red-dark">Keeping it 100</h1>
+                </div>
+                <p class="is-size-4 has-text-weight-normal has-text-grey-light">We believe that our work and ethics
                     define who we are. We're Human-centered Designers and Developers behind some of North America's
                     biggest mobile apps and digital brands, looking to make a net-positive impact in the world.</p>
             </div>
             <div class="content">
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">Our approach to building software
+                <p class="is-size-4 has-text-weight-normal has-text-grey-light">Our approach to building software
                     prioritizes accessibility, user-centric design, and inclusion. We research and generate ideas, then
                     build, test, and iterate on them using real-world analytics and actionable KPIs. The emphasis on
                     data-driven design, diversity, user empathy, and sustainability aren't buzzwords, they're why we
