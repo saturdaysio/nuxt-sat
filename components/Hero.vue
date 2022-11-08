fe6d1e3e3b5849f01343b29e8147471989a8b356
pchae <20016424+pchae@users.noreply.github.com>
2022-11-08 03:19:18 -0500

    center text and change size

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="container mt-6 py-6 px-4">
     <div class="content is-medium">
-      <h1 class="title has-text-white">
+      <h1 class="title is-size-1 has-text-centered has-text-white">
         Saturdays is a Canadian digital studio based in Toronto
       </h1>
-      <h2 class="is-size-5 has-text-weight-normal has-text-grey-light">
+      <h2 class="is-size-2 has-text-centered has-text-weight-normal has-text-grey-light">
         We're a Data-driven digital studio that focuses on the Design, Development, and Strategy of digital products.
       </h2>
     </div>
