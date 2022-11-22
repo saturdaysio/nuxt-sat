8dcb97513aa96540ebb3769646379ef4956cad42
pchae <20016424+pchae@users.noreply.github.com>
2022-11-21 21:46:50 -0500

    refactor ethics layout

--- pages/about.vue
+++ pages/about.vue
@@ -18,19 +18,19 @@
 
         <section id="ethics" class="container my-6 py-6 px-4">
             <div class="content">
-                <div class="content is-medium">
+                <div class="content">
                     <h1 class="is-size-2 has-text-centered text-gradient-blue-red-dark">Keeping it 100</h1>
-                </div>
-                <p class="is-size-5 has-text-weight-normal has-text-grey-light">We believe that our work and ethics
+                    <p class="is-size-5 has-text-weight-normal has-text-grey-light">We believe that our work and ethics
                     define who we are. We're Human-centered Designers and Developers behind some of North America's
                     biggest mobile apps and digital brands, looking to make a net-positive impact in the world.</p>
-            </div>
-            <div class="content">
+                </div>
+                <div class="content">
                 <p class="is-size-5 has-text-weight-normal has-text-grey-light">Our approach to building software
                     prioritizes accessibility, user-centric design, and inclusion. We research and generate ideas, then
                     build, test, and iterate on them using real-world analytics and actionable KPIs. The emphasis on
                     data-driven design, diversity, user empathy, and sustainability aren't buzzwords, they're why we
                     win.</p>
+            </div>
             </div>
         </section>
 
