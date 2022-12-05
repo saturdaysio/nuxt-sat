5a08182a08c60b05a372ef874070074a6645e5c9
pchae <20016424+pchae@users.noreply.github.com>
2022-12-04 23:28:20 -0500

    update template and script to use cssModule $style

--- pages/tos.vue
+++ pages/tos.vue
@@ -7,23 +7,23 @@
 
     <Nav />
 
-    <section id="legal" class="container mt-6 py-6 px-4">
-      <div class="content is-medium">
-        <h1 class="title is-size-3-mobile has-text-white">Terms of Service</h1>
-        <p class="subtitle has-text-grey-light">Last updated: Jan 25, 2022</p>
+    <section id="legal" :class="[$style.container, $style.mt6, $style.py6, $style.px4]">
+      <div :class="[$style.content, $style.isMedium]">
+        <h1 :class="[$style.title, $style.isSize3Mobile, $style.hasTextWhite]">Terms of Service</h1>
+        <p :class="[$style.subtitle, $style.hasTextGreyLight]">Last updated: Jan 25, 2022</p>
       </div>
-      <div id="definition" class="content is-normal">
-        <p class="has-text-grey-light pb-2">
+      <div id="definition" :class="$style.content, $style.isNormal">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           Saturdays.io, Inc. (“our,” “we,” or “us”) offers you access to mobile applications, websites, and real-time
           API services. These Terms of Service (“Terms”) apply to you as a user (“you” or “User”) and create a legal,
           binding contract between you and us regarding your access to or use of our applications, websites, content,
           features, products or other services (collectively, “Services”).
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           Please read these Terms carefully. Your access to and use of the Services are impacted by these Terms and if
           you do not agree to any of these Terms, you may not access or use the Services.
         </p>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           Your privacy is important to us. Our Privacy Policy describes the types of information we collect from you,
           what we do with it and how we might share it. You can read it here <NuxtLink to="https://saturdays.io/privacy"
             rel="noreferrer" target="_blank">https://saturdays.io/privacy</NuxtLink>. Your access and use of the
@@ -31,11 +31,11 @@
         </p>
       </div>
 
-      <div id="use-of-services" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="use-of-services" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           1. Use of Services
         </h1>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           You may access and/or use the Services only if you have reached the age of majority or legal age in your
           jurisdiction (generally 18 or older) and can form legally binding contracts under applicable law. If you are
           under 18 or the legal age of majority in your jurisdiction, your parent or guardian must agree to these Terms
@@ -45,12 +45,12 @@
           inability to access or use the Services. You agree that all activity engaged in by any account associated with
           you as part of the Services will be your responsibility.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           The Services are controlled and offered by us from Ontario, Canada. We do not make any representations that
           the Services are appropriate in your location. If you use the Services from any country other than Canada, it
           is your responsibility to comply with your local law.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           Your use of the Services requires that you follow certain standards of conduct. We reserve the right in our
           sole discretion to disallow or remove any user from accessing or using the Services in the event we deem such
           standards of conduct are not being followed. By accessing or using the Services, you agree to not engage in
@@ -58,123 +58,123 @@
         </p>
         <ol type="1">
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Violate any law, regulation or other right that may be subject to any criminal or civil penalty.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Violate, infringe or otherwise use the intellectual property rights of a third party without permission.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Violate or infringe on the publicity rights, privacy rights or other rights of third parties.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Engage in the use of spyware or other software designed to collect and harvest data from us or the
               Services.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Communicate with users of the Services for purposes of distributing malicious files, software or other
               programs meant to disrupt the use of computer systems or related technology, or for the purposes of
               “phishing” and other fraudulent activity.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Harass, stalk, threaten, or otherwise harm another individual.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Reverse engineer any of the software or underlying code used by us or in the Services.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Impersonate any person or company, including, but not limited to, our employees, contractors or other
               personnel, or misrepresent your affiliation with a person or company.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Create and register more than one profile associated with the Services, including registering a profile on
               behalf of an individual other than yourself.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Encourage or otherwise assist any other party to do any of the acts previously described in this section.
             </p>
           </li>
         </ol>
       </div>
 
-      <div id="service-disruption-terminate" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="service-disruption-terminate" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           2. Service Disruptions and Terminating Use of the Services
         </h1>
-        <p class="has-text-grey-light pb1">
+        <p :class="[$style.hasTextGreyLight, $style.pb1]">
           Our Services may be interrupted at any time for repairs, maintenance, upgrades or similar. At any time, we may
           discontinue or change any of the Services that may affect certain devices and platforms used for accessing and
           using the Services.
         </p>
-        <p class="has-text-grey-light pb1">
+        <p :class="[$style.hasTextGreyLight, $style.pb1]">
           You can stop using the Services at any time. If you’d like to know how we handle the information you provide,
           please refer to the Privacy Policy <NuxtLink to="https://saturdays.io/privacy" rel="noreferrer"
             target="_blank">https://saturdays.io/privacy</NuxtLink>.
         </p>
-        <p class="has-text-grey-light pb1">
+        <p :class="[$style.hasTextGreyLight, $style.pb1]">
           We reserve the right to suspend or terminate any User without notice, for any reason in our sole discretion,
           including breach of these Terms.
         </p>
-        <p class="has-text-grey-light pb1">
+        <p :class="[$style.hasTextGreyLight, $style.pb1]">
           If we disable your account for a violation of these Terms or the Privacy Policy, you shall not create another
           account without our permission.
         </p>
       </div>
 
-      <div id="partners-third-party" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="partners-third-party" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           3. Partners and third-parties
         </h1>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           While using the Services, you may use, access, or otherwise interact with our partners and other third-parties
           including their websites, apps, products, services, content and other properties. In such cases, the terms
           governing such relationship will be between you and such partner and/or third-party.
         </p>
       </div>
 
-      <div id="ownership" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="ownership" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           4. Ownership and Rights
         </h1>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           We own all rights, trademarks, trade secrets, patents and any other intellectual property that may be
           associated with or created by us as part of our Services that were not developed by a third-party. You may not
           use such intellectual property for any reason without our explicit consent.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           We do not own any of the information that you disseminate while using the Services. You are responsible for
           any possible infringements of intellectual property that may be caused by dissemination of such information.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           You agree to grant us a worldwide, non-exclusive, royalty-free, sublicensable, and transferable license to
           use, reproduce, distribute, create derivative works of, display, and perform the information that you upload,
           submit, store, send, or receive on or through our Services. This license to us is limited only to our enabling
           you to use our Services according to these Terms, and you grant us no other license.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           We grant you a limited, revocable, non-exclusive, non-sublicensable, and non-transferable license to use our
           Services as long as you are in compliance with these Terms. This license is limited only to enabling you to
           use our Services according to these Terms, and you are granted no other license by us.
         </p>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           We reserve the right to address claims of intellectual property infringement by you or a third-party when
           using our Services by such reasonable means to protect the rights of the intellectual property owner. If you
           would like to report intellectual property infringement please contact us at <NuxtLink
@@ -182,11 +182,11 @@
         </p>
       </div>
 
-      <div id="misc" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="misc" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           5. Miscellaneous
         </h1>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           Disclaimer and Limitation of Liability: YOU USE OUR SERVICES AT YOUR OWN RISK AND SUBJECT TO THE FOLLOWING
           DISCLAIMERS. THE SERVICES ARE PROVIDED ON AN “AS IS” BASIS WITH NO EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
           BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE, NON-INFRINGEMENT,
@@ -204,7 +204,7 @@
           DOES NOT KNOW OR SUSPECT TO EXIST IN HIS OR HER FAVOR AT THE TIME OF EXECUTING THE RELEASE, WHICH IF KNOWN BY
           HIM OR HER MUST HAVE MATERIALLY AFFECTED HIS OR HER SETTLEMENT WITH THE DEBTOR.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           THE SATURDAYS PARTIES ARE NOT LIABLE TO YOU FOR ANY LOST PROFITS OR CONSEQUENTIAL, SPECIAL, PUNITIVE,
           INDIRECT, OR INCIDENTAL DAMAGES RELATING TO, ARISING OUT OF, OR IN ANY WAY IN CONNECTION WITH OUR TERMS, US,
           OR OUR SERVICES, EVEN IF THE SATURDAYS PARTIES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. OUR
@@ -216,19 +216,19 @@
           NOTWITHSTANDING ANYTHING TO THE CONTRARY IN OUR TERMS, IN SUCH CASES, THE LIABILITY OF THE SATURDAYS PARTIES
           WILL BE LIMITED TO THE FULLEST EXTENT PERMITTED BY APPLICABLE LAW.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           Indemnification: You agree to indemnify and hold us harmless, including our employees, directors, officers,
           agents, third party providers including service providers, advertising partners and any other partners that
           may implement our Services in its organization against all damages, expenses, claims, losses, liabilities,
           legal fees, attorney fees and any other costs incurred by such parties associated with your violation of these
           Terms.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           No assignment: These Terms and all rights and obligations ascribed to you hereunder, and/or your accounts
           associated with the Services, are personal to you and may not be transferred or assigned by you at any time,
           in any way, without consent. We may assign our rights under these Terms at any time without your consent.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           Choice of Law and Arbitration: These Terms, except to the extent not otherwise governed by the laws of Canada,
           shall be governed by the laws of the province of Ontario without giving effect to principles of conflicts of
           law. Any dispute arising from these Terms shall be subject to and settled within Ontario, in accordance with
@@ -241,7 +241,7 @@
           BY AGREEING TO THESE TERMS BOTH YOU AND SATURDAYS.IO, INC. WAIVE THE RIGHT TO TRIAL BY JURY OR TO PARTICIPATE
           IN A CLASS ACTION.
         </p>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           Entire Agreement, Severability, Waiver: These Terms contain the entire understanding of the parties with
           respect to the subject matter hereof and supersede all prior agreements and understandings, oral or written,
           with respect to such matters, which the parties acknowledge, if applicable, have been merged into these Terms.
@@ -261,8 +261,18 @@
 
 
 <script lang="ts">
+  import { useCssModule } from 'vue'
+
+  export default {
+      setup() {
+          useCssModule()
+          const $style = useCssModule()
+      }
+  }
 </script>
 
 
-<style scoped>
+<style lang="scss" module scoped>
+@import '~/assets/styles/main.module.scss';
+
 </style>
