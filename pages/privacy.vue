2e7e61568d3d5b9547fda1d61b0e5edcab84ae92
pchae <20016424+pchae@users.noreply.github.com>
2022-12-04 23:22:17 -0500

    update template and script for cssModules $style

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -7,13 +7,13 @@
 
     <Nav />
 
-    <section id="legal" class="container mt-6 py-6 px-4">
-      <div class="content is-medium">
-        <h1 class="title is-size-3-mobile has-text-white">Privacy Policy</h1>
-        <p class="subtitle has-text-grey-light">Last updated: Jan 25, 2022</p>
+    <section id="legal" :class="[$style.container, $style.mt6, $style.py6, $style.px4]">
+      <div :class="[$style.content, $style.isMedium]">
+        <h1 :class="[$style.title, $style.isSize3Mobile, $style.hasTextWhite]">Privacy Policy</h1>
+        <p :class="[$style.subtitle, $style.hasTextGreyLight]">Last updated: Jan 25, 2022</p>
       </div>
-      <div id="definition" class="content is-normal">
-        <p class="has-text-grey-light pb-2">
+      <div id="definition" :class="[$style.content, $style.isNormal]">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           Saturdays.io, Inc. (“our,” “we,” or “us”), takes your (“you,” “your,” or the “User”) privacy seriously. We may
           collect, use, and share some of your personal information. This Privacy Policy is a guide to explain how we do
           that, and your rights as a User. This Privacy Policy forms part of our Terms of Service available at <NuxtLink
@@ -22,91 +22,91 @@
 
       </div>
 
-      <div id="use-of-services" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="use-of-services" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           1. Policy Scope
         </h1>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           This Privacy Policy covers our treatment of your personally identifiable information and any other information
           you may provide us when using our applications, websites, content, features, products or other services
           (referred to herein as the “Services”).
         </p>
       </div>
 
-      <div id="info-collected" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="info-collected" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           2. Information We Collect
         </h1>
-        <p class="has-text-grey-light ">
+        <p :class="$style.hasTextGreyLight">
           We focus on using your personal information to provide and improve our Services, respond to your requests,
           service user accounts connected to our Services, provide you with services or information you may request,
           communicate with you, and inform you about new products and services that may be of interest and to improve
           our site. We collect the following types of information:
         </p>
 
-        <ol class="1">
+        <ol type="1">
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Information You Provide Us: Name, mailing address, phone number, email address, contact preferences,
               device identifiers, IP address, location information, credit card information, and profile information
               where the contact is via social media.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Information We Collect from Users of our Services: Name, mailing address, Phone number, Email address,
               Contact preferences, Device identifiers, IP address, and location information.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Information We Collect from Third Parties: We don't collect information from third parties.
             </p>
           </li>
         </ol>
       </div>
 
-      <div id="how-we-use-info" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="how-we-use-info" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           3. How We Use the Information We Collect
         </h1>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           We may use the information that we collect for any of the following purposes:
         </p>
 
-        <ol class="1">
+        <ol type="1">
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Improve Service functionality and efficiency
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Provide, maintain and improve our Services
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Provide assistance to users
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Personalize our Services
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Measure and better understand the performance of our Services
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Communicate with Users
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Protect Users, the community, us and other legal reasons such as complying with applicable laws, assisting
               with law enforcement agencies if legally mandated, to find instances of abuse or misuse of our services or
               illegal activity, maintain security and safety when using our Services
@@ -114,80 +114,80 @@
           </li>
         </ol>
 
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           In the event that we determine we need to use your personal information for a reason not included in this
           Privacy Policy, we will ask for your consent first.
         </p>
       </div>
 
-      <div id="share-info" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="share-info" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           4. Sharing Information
         </h1>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           We do not sell your personal information. To better provide Services, we may share your personal information
           with third parties (like social networks), and our service providers.
         </p>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           We may share your information with third parties in other cases such as:
         </p>
 
         <ol type="1">
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               If a government body requests information and we deemed it to be necessary to disclose such information in
               order to comply with applicable laws and legal processes;
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               To cooperate with law enforcement or other government authorities as it relates to any investigations or
               other inquiries to the extent, we deem it necessary to protect either our users, the public, or us.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               To defend or pursue legal claims as required or permitted by law.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Enforcing our Terms of Service.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               Responding to any legal process including court orders and search warrants.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               To detect or protect against fraud, security, technical or legal issues associated with the use of our
               Services.
             </p>
           </li>
           <li>
-            <p class="has-text-grey-light">
+            <p :class="$style.hasTextGreyLight">
               At any time that you consent to such sharing.
             </p>
           </li>
         </ol>
       </div>
 
-      <div id="control-info" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="control-info" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           5. Controlling Your Information
         </h1>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           You have control over the information that you share with us subject to certain limitations.
         </p>
-        <p class="has-text-grey-light pb-2">
+        <p :class="[$style.hasTextGreyLight, $style.pb2]">
           Our Services have been built in a way to minimize the amount of personal information that is stored within
           them. Nonetheless, if you want to review, update, change or delete the information that you have shared with
           us, including deletion of your account, you may send a request to do so to <NuxtLink
             to="mailto:support@saturdays.io">support [at] saturdays.io</NuxtLink>.
         </p>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           Even in the event that we delete your personal information, we still may retain some information for
           legitimate business purposes such as maintenance of security of the Services or to comply with applicable laws
           or regulations, depending on the circumstances of such deletion. You understand and agree that we may have
@@ -197,42 +197,42 @@
         </p>
       </div>
 
-      <div id="do-not-track" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="do-not-track" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           6. Do Not Track
         </h1>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           We do not track our Users over time and across third party websites and therefore our Services do not respond
           to Do Not Track signals.
         </p>
       </div>
 
-      <div id="gdpr" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="gdpr" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           7. GDPR
         </h1>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           We do not collect “personal data” about, or monitor behavior of, “data subjects” as those terms are defined in
           GDPR Art. 4(1). We do not “target” individuals in the EEU.
         </p>
       </div>
 
-      <div id="age-restrictions" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="age-restrictions" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           8. Age Restrictions
         </h1>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           We do not knowingly collect or solicit the personal information of anyone under the age of 13, even with a
           parent or guardian's consent. Our services are not intended for children and in the event that we determine
           that a person under 13 has provided personal information, we will delete that information immediately.
         </p>
       </div>
 
-      <div id="do-not-track" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="do-not-track" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           9. Changes to this Privacy Policy
         </h1>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           We may update this Privacy Policy from time to time as necessary based on changes to our company, our
           Services, or to applicable laws. In the event that we do update this Privacy Policy, such changes will be made
           online. You will be notified of any material changes through the Services or e-mail. Your use of the Services
@@ -240,11 +240,11 @@
         </p>
       </div>
 
-      <div id="assignment" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="assignment" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           10. Assignment, Change of Control and Transfer
         </h1>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           All of our rights and obligations pursuant to this Privacy Policy are freely assignable by us to any of our
           affiliates, in connection with a merger, acquisition, restructuring, or sale of assets, or by operation of law
           or otherwise, and we may transfer your information to any of our affiliates, successor entities, or a new
@@ -252,11 +252,11 @@
         </p>
       </div>
 
-      <div id="contact-us" class="content is-normal py-6">
-        <h1 class="is-size-3 is-size-4-mobile has-text-weight-bold has-text-white">
+      <div id="contact-us" :class="[$style.content, $style.isNormal, $style.py6]">
+        <h1 :class="[$style.isSize3, $style.isSize4Mobile, $style.hasTextWeightBold, $style.hasTextWhite]">
           11. Contact Us
         </h1>
-        <p class="has-text-grey-light">
+        <p :class="$style.hasTextGreyLight">
           For any questions or concerns that you may have regarding this Privacy Policy, please contact us at <NuxtLink
             to="mailto:support@saturdays.io">support [at] saturdays.io</NuxtLink>.
         </p>
@@ -270,8 +270,18 @@
 
 
 <script lang="ts">
+  import { useCssModule } from 'vue'
+
+  export default {
+      setup() {
+          useCssModule()
+          const $style = useCssModule()
+      }
+  }
 </script>
 
 
-<style scoped>
+<style lang="scss" module scoped>
+@import '~/assets/styles/main.module.scss';
+
 </style>
