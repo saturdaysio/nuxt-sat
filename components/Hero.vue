656eb0e1434765b1308c10fcf60f9a6c7e58cdaa
pchae <20016424+pchae@users.noreply.github.com>
2022-12-13 00:02:36 -0500

    update font size

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container my-16 py-8 px-4">
     <div class="content">
-      <h1 class="text-2xl text-center font-bold text-white">
+      <h1 class="text-4xl text-center font-bold text-white">
         Saturdays is a Canadian digital studio based in Toronto
       </h1>
     </div>
