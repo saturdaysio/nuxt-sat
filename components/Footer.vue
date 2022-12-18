36b25b39dec9fd98b31d2a75aabe321d5f81a090
pchae <20016424+pchae@users.noreply.github.com>
2022-12-18 16:43:39 -0500

    Fixed template formatting

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,32 +1,37 @@
 <template>
     <footer class=" w-full mx-auto">
         <div class="footer">
-        <div class="flex justify-between">
-            <div class="copyright flex items-center">
+            <div class="flex justify-between">
+                <div class="copyright flex items-center">
                     <p class="text-white">
                         &#169; 2016-2022 Saturdays.io
                     </p>
                 </div>
-            <div class="flex flex-end">
-                <div class="nav-item">
-                    <NuxtLink to="/tos" active-class="active" class="px-4 py-2 flex items-center text-sm uppercase font-bold leading-snug hover:opacity-75">
-                        <strong>Terms</strong>
-                    </NuxtLink>
+                <div class="flex flex-end">
+                    <div class="nav-item">
+                        <NuxtLink to="/tos" active-class="active"
+                            class="px-4 py-2 flex items-center text-sm uppercase font-bold leading-snug hover:opacity-75">
+                            <strong>Terms</strong>
+                        </NuxtLink>
 
-                </div>
-                <div class="nav-item">
-                    <NuxtLink to="/privacy" active-class="active" class="px-4 py-2 flex items-center text-sm uppercase font-bold leading-snug hover:opacity-75">
-                        <strong>Privacy</strong>
-                    </NuxtLink>
+                    </div>
+                    <div class="nav-item">
+                        <NuxtLink to="/privacy" active-class="active"
+                            class="px-4 py-2 flex items-center text-sm uppercase font-bold leading-snug hover:opacity-75">
+                            <strong>Privacy</strong>
+                        </NuxtLink>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     </footer>
+
 </template>
+
 
 <script lang="ts">
 </script>
+
 
 <style lang="scss" scoped>
 @import '~/assets/styles/tailwind.scss';
@@ -34,5 +39,4 @@
 footer {
     padding: 4rem 1rem;
 }
-
 </style>
