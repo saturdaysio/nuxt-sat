3f61d37c054c72d09bc34c579be791d1516d5481
pchae <20016424+pchae@users.noreply.github.com>
2022-12-20 23:22:45 -0500

    update layout

--- error.vue
+++ error.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col h-screen justify-between bg-black">
+    <div class="bg-black">
 
         <Head>
             <Title>Saturdays.io - Page not found</Title>
@@ -9,35 +9,36 @@
         <NavBar />
 
         <div class="container lg:max-w-5xl mx-auto px-4">
-            <div class="flex flex-col justify-center">
-                <div class="pb-8">
-                    <div class="text-center pb-2">
-                        <h1 class="text-4xl font-bold text-grad-01">Page not found</h1>
+            <div class="py-20">
+                <div class="flex flex-col justify-center">
+                    <div class="pb-8">
+                        <div class="text-center pb-2">
+                            <h1 class="text-4xl font-bold text-grad-01">Page not found</h1>
+                        </div>
+                        <div class="text-center">
+                            <p class="text-xl text-white">The page you are looking for does not exist.</p>
+                        </div>
                     </div>
-                    <div class="text-center">
-                        <p class="text-xl text-white">The page you are looking for does not exist.</p>
-                    </div>
-                </div>
 
-                <div class="flex justify-center">
-                    <NuxtLink to="/">
-                        <button class="button is-link">
-                            <p class="text-xl"><strong>Go home</strong></p>
-                        </button>
-                    </NuxtLink>
+                    <div class="flex justify-center">
+                        <NuxtLink to="/">
+                            <button class="button is-link">
+                                <p class="text-xl"><strong>Go home</strong></p>
+                            </button>
+                        </NuxtLink>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <Footer />
     </div>
 </template>
 
 
 <script lang="ts">
-    export default {
+export default {
     layout: 'error',
-    }
+}
 </script>
 
 
