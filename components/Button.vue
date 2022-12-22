c9237c2518c3b2131b053eaeae69d9188eb96bb3
pchae <20016424+pchae@users.noreply.github.com>
2022-12-21 20:37:18 -0500

    create new component

---
+++ components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <NuxtLink>
+        <button class="button is-link">
+            <p class="text-xl"><strong>{{ cta }}</strong></p>
+        </button>
+    </NuxtLink>
+</template>
+
+
+<script lang="ts" setup>
+    const props = defineProps({
+        cta: String,
+    })
+</script>
+
+
+<style lang="scss" scoped>
+@import '~/assets/styles/tailwind.scss';
+
+.button.is-link {
+    color: $white;
+    background: none;
+    border-image: linear-gradient(120deg, rgba(248, 12, 128), rgba(60, 120, 244)) 1;
+    border-width: 4px;
+    border-style: solid;
+    padding: 1.5rem 2rem;
+
+    &:hover {
+        background: none;
+        opacity: 0.75;
+    }
+}
+</style>
