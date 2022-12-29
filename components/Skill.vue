d7adb41e8a3a62a43f3fa8cff8163c5e66d8f8b2
pchae <20016424+pchae@users.noreply.github.com>
2022-12-29 14:35:14 -0500

    lint template and reduce titles font size

--- components/Skill.vue
+++ components/Skill.vue
@@ -2,54 +2,52 @@
     <section id="skills" class="container lg:max-w-7xl mx-auto px-4 py-16">
         <div class="flex flex-col">
             <div class="pb-8">
-                        <h1 class="text-3xl lg:text-5xl text-center font-bold text-white">
-                            What we do
-                        </h1>
-                    </div>
-                    <div class="flex flex-wrap">
-                        <div class="group flex flex-col py-8 px-6" v-for="item in skillStore.skillJson.service" :key="item.id">
-                            <h1 class="text-2xl lg:text-4xl font-bold text-grad-01">{{ item.title }}</h1>
-                            <ul>
-                                <li class="text-xl text-white" v-for="skill in item.list" :key="skill.li">{{ skill.li }}</li>
-                            </ul>
-                        </div>
-                    </div>
+                <h1 class="text-3xl lg:text-5xl text-center font-bold text-white">
+                    What we do
+                </h1>
+            </div>
+            <div class="flex flex-wrap">
+                <div class="group flex flex-col py-8 px-6" v-for="item in skillStore.skillJson.service" :key="item.id">
+                    <h1 class="text-2xl lg:text-3xl font-bold text-grad-01">{{ item.title }}</h1>
+                    <ul>
+                        <li class="text-xl text-white" v-for="skill in item.list" :key="skill.li">{{ skill.li }}</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </section>
 </template>
 
 <script lang="ts">
-    import { useSkillStore } from "~~/store/skill";
+import { useSkillStore } from "~~/store/skill";
 
-    export default {
-        setup() {
-            const skillStore = useSkillStore()
-            return { skillStore }
-        }
+export default {
+    setup() {
+        const skillStore = useSkillStore()
+        return { skillStore }
     }
+}
 </script>
 
 <style lang="scss" scoped>
+.group {
+    display: flex;
+    align-items: flex-start;
+    width: 25%;
+    flex-shrink: 0;
+}
 
+
+@media only screen and (max-width: 1023px) {
     .group {
-        display: flex;
-        align-items: flex-start;
-        justify-content: center;
-        width: 25%;
-        flex-shrink: 0;
+        width: 50%;
     }
 
+}
 
-    @media only screen and (max-width: 1023px) {
-        .group {
-            width: 50%;
-        }
-
+@media only screen and (max-width: 640px) {
+    .group {
+        width: 100%;
     }
-
-    @media only screen and (max-width: 640px) {
-        .group {
-            width: 100%;
-        }
-    }
+}
 </style>
