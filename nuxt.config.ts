217cea1d4f894e2e06471b980dc8eebe580d4431
pchae <20016424+pchae@users.noreply.github.com>
2023-01-06 00:10:30 -0500

    update google font url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
                 // Add Google fonts resource
                 { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
                 { rel: 'preconnect', href: 'https://fonts.gstatic.com'},
-                { href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700;900&display=swap'}
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Poppins:wght@100;300;400;700;900&display=swap'}
               ]
         },
     },
