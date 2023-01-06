9ad3c876bf84749b2ea7dfc4c6defce73b4e4a54
pchae <20016424+pchae@users.noreply.github.com>
2023-01-06 03:35:39 -0500

    replace head tags with useHead composable and add error props

--- error.vue
+++ error.vue
@@ -1,26 +1,21 @@
 <template>
     <div class="flex flex-col h-screen justify-between">
 
-        <Head>
-            <Title>Saturdays.io - Error</Title>
-            <Meta name="description" content="Saturdays.io - 404 page not found" />
-        </Head>
-
         <NavBar />
 
         <section class="container lg:max-w-5xl mx-auto px-8">
             <div class="flex flex-col justify-center">
                 <div class="mb-8">
                     <div class="text-center pb-2">
-                        <h1 class="text-4xl font-thin text-grad-01">Page not found</h1>
+                        <h1 class="text-4xl font-bold text-grad-01">ERROR {{ error.statusCode }}</h1>
                     </div>
                     <div class="text-center">
-                        <p class="text-xl text-white">The page you are looking for does not exist.</p>
+                        <p class="text-xl text-white">{{ error.message }}</p>
                     </div>
                 </div>
 
                 <div class="flex justify-center">
-                    <Button :cta="'Go Home'" to="/" />
+                    <Button :cta="'Go Home'" @click="handleClearError" />
                 </div>
             </div>
         </section>
@@ -31,7 +26,18 @@
 </template>
 
 
-<script lang="ts">
+<script lang="ts" setup>
+    defineProps(['error'])
+
+    const handleClearError = () => clearError({ redirect: '/' })
+
+    useHead({
+        title: 'Saturdays.io - ERROR',
+        meta: [
+            { name: 'description', content: 'Error page' }
+        ]
+    })
+
 </script>
 
 
