8d8d712737e5ae3af86c27383bcf722444b4e2d2
pchae <20016424+pchae@users.noreply.github.com>
2023-01-06 00:49:01 -0500

    update font weight

--- error.vue
+++ error.vue
@@ -12,7 +12,7 @@
             <div class="flex flex-col justify-center">
                 <div class="mb-8">
                     <div class="text-center pb-2">
-                        <h1 class="text-4xl font-bold text-grad-01">Page not found</h1>
+                        <h1 class="text-4xl font-thin text-grad-01">Page not found</h1>
                     </div>
                     <div class="text-center">
                         <p class="text-xl text-white">The page you are looking for does not exist.</p>
