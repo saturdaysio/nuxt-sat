5b0450240098defec06649cef2998ddfed76f3e1
pchae <20016424+pchae@users.noreply.github.com>
2023-01-08 20:28:37 -0500

    initial commit

---
+++ pages/login.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="flex flex-col h-screen justify-between">
+
+        <NavBar />
+
+        <section id="default" class="mx-auto px-4">
+            <div class=" sm:w-96">
+                <form class="rounded px-6 py-8 bg-black-500">
+                    <h1 class="text-3xl lg:text-4xl text-center font-bold text-grad-01 pb-8">
+                        Sign up
+                    </h1>
+
+                    <div id="username" class="mb-8">
+                        <label class="block text-white text-sm font-bold mb-2" for="username">
+                            Username
+                        </label>
+                        <input id="username" type="text" placeholder="Username" 
+                            class=" appearance-none border rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none">
+                    </div>
+
+                    <div id="password" class="mb-8">
+                        <label class="block text-white text-sm font-bold mb-2" for="password">
+                            Password
+                        </label>
+                        <input id="password" type="current-password" placeholder="********" 
+                            class=" appearance-none border rounded w-full py-2 px-4 text-gray-700 mb-2 leading-tight focus:outline-none">
+                        <div id="error">
+                            <p class="text-red-500 text-xs italic">Password is required.</p>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col items-center justify-between">
+                        <button class="button cta w-full rounded font-bold mb-4" type="button">
+                            Sign In
+                        </button>
+
+                        <a class="inline-block align-baseline text-sm text-blue hover:text-purple-800" href="/">
+                            Forgot Password?
+                        </a>
+                    </div>
+                </form>
+
+            </div>
+        </section>
+
+        <Footer />
+
+    </div>
+</template>
+
+
+<script lang="ts" setup>
+
+useHead({
+    title: 'Saturdays.io - Sign in',
+    meta: [
+        { name: 'description', content: 'Saturdays.io sign in pagae' },
+        { name: 'keywords', content: 'Saturdays.io, Digital, Studio, Creative, Digital Studio, Creative Studio,' },
+    ]
+})
+
+</script>
+
+
+<style lang="scss">
+
+</style>
