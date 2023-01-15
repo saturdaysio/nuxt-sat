348b0017a56f90bd9b0ecd3c8ecc082e4436ee8a
pchae <20016424+pchae@users.noreply.github.com>
2023-01-14 23:05:50 -0500

    add horizontal line to layout

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,11 @@
 <template>
     <footer class="w-full mx-auto px-4 py-8 bg-black-900">
         <div class="lg:container mx-auto">
+            <hr class="w-full h-px mx-auto my-4 border-0 rounded md:my-10 bg-gray-900">
             <div class="flex flex-col-reverse items-center md:flex-row justify-between">
                 <div id="copyright" class="flex items-center px-4">
                     <p class="text-white">
-                        &#169; Saturdays.io Inc. 2016 - 2023
+                        &#169; 2016 - 2023 Saturdays.io Inc. All rights reserved.
                     </p>
                 </div>
                 <div class="flex flex-end pb-4 md:pb-0">
