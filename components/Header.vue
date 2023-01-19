4ba8c046f66b2ea94292659c0fbed9465b096157
pchae <20016424+pchae@users.noreply.github.com>
2023-01-19 02:36:05 -0500

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="w-full mx-auto py-16 bg-purple-900">
+        <div class="relative flex flex-col justify-center items-center mx-auto ">
+            <div class="z-10 absolute container lg:max-w-5xl items-center px-4">
+                <h1 class="text-purple-pink">
+                    We're a Data-driven digital studio that focuses on the Design, Development and Strategy of digital
+                    products.
+                </h1>
+            </div>
+            <div class="w-full flex">
+                <picture>
+                    <source type="image/png" srcset="~/assets/img/git.webp">
+                    <img src="~/assets/img/git.webp" alt="Mobile app interfaces" draggable="false" loading="lazy"
+                        width="1163" height="600">
+                </picture>
+            </div>
+        </div>
+    </section>
+</template>>
+
+
+<script lang="ts">
+</script>
+
+
+<style lang="scss">
+</style>
