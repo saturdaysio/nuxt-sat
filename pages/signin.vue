c7d2c4b492f4ea2713e3531dbf023f09af5a0d2a
pchae <20016424+pchae@users.noreply.github.com>
2023-01-29 17:17:58 -0500

    clean up template and script

--- pages/signin.vue
+++ pages/signin.vue
@@ -7,7 +7,7 @@
 
             <div class="">
                 <form
-                    @submit.prevent="() => (isSignUp ? signUp() : signIn ())"
+                    @submit.prevent=""
                     class="rounded px-6 py-8 bg-black-500">
 
                     <h1 class="text-3xl lg:text-4xl text-center font-bold text-grad-01 pb-8">
@@ -18,16 +18,14 @@
                         <label class="block text-white text-sm font-bold mb-2" for="email">
                             Email
                         </label>
-                        <input id="email" type="email" placeholder="Email address" v-model="email"
-                            class="appearance-none border rounded w-full py-2 px-4 text-gray-700 focus:outline-none">
+                        <input id="email" type="email" placeholder="Email address" v-model="email" class="appearance-none border rounded w-full py-2 px-4 text-gray-700 focus:outline-none">
                     </div>
 
                     <div id="password" class="mb-8">
                         <label class="block text-white text-sm font-bold mb-2" for="password">
                             Password
                         </label>
-                        <input id="password" type="current-password" placeholder="********" v-model="password" 
-                            class=" appearance-none border rounded w-full py-2 px-4 text-gray-700 mb-2 focus:outline-none">
+                        <input id="password" type="current-password" placeholder="********" v-model="password" class=" appearance-none border rounded w-full py-2 px-4 text-gray-700 mb-2 focus:outline-none">
                     </div>
 
                     <div class="flex flex-col items-center justify-between">
@@ -49,6 +47,7 @@
 
 <script setup lang="ts">
 
+    // Page meta info
     useHead({
         title: 'Saturdays.io - Sign in',
         meta: [
@@ -60,15 +59,13 @@
 
     // Supabase code
     const client = useSupabaseClient()
-    const user = useSupabaseUser()
+
+    // useSupabaseUser doesn't redirect to path without page reload
+    const user = useSupabaseAuthClient()
     const email = ref('')
     const password = ref('')
     const isSignUp = ref(false)
 
-
-    // Sign up function
-    const signUp = async () => {
-    }
 
     // Sign in function
     const signIn = async () => {
@@ -82,6 +79,7 @@
     // Watch user state
     onMounted(() => {
         watchEffect(() => {
+            // Don't use user for this
             if (user.value) {
                 navigateTo('/secret')
             }
