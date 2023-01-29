76e453f780852d2432564a87d9d5a3c55ddb6082
pchae <20016424+pchae@users.noreply.github.com>
2023-01-28 21:15:14 -0500

    create mural bg component

---
+++ components/MenuBG.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Background container-->
+    <div id="canvas" class="fixed w-full h-screen">
+        <!-- Comment -->
+        <div id="container" class="flex flex-col justify-between h-full">
+            <div id="top-right" class="gird flex-row w-full">
+                <img src="@/assets/img/block-corner-tr.svg" draggable="false" height="200px" width="100%">
+            </div>
+
+            <div id="bot-left" class="flex-end">
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+<script setup lang="ts">
+</script>
+
+
+<style lang="css" scoped>
+</style>
