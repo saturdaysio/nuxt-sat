e1c213a1fb235a4bc471cce940ab5f90e5daed36
pchae <20016424+pchae@users.noreply.github.com>
2023-01-31 02:52:46 -0500

    remove padding on NuxtLinks

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,23 +1,23 @@
 <template>
-    <footer class="w-full mx-auto px-4 py-8 bg-black-900">
-        <div class="lg:container mx-auto">
+    <footer class="w-full mx-auto bg-black-900">
+        <div class="lg:container px-4 py-8 ">
             <hr class="w-full h-px mx-auto my-4 border-0 rounded md:my-10 bg-gray-900">
             <div class="flex flex-col-reverse items-center md:flex-row justify-between">
-                <div id="copyright" class="flex items-center px-4">
+                <div id="copyright" class="flex items-center">
                     <p class="text-white">
-                        &#169; 2016 - 2023 Saturdays.io Inc. All rights reserved.
+                        &#169; 2016 - 2023 Saturdays.io All rights reserved.
                     </p>
                 </div>
                 <div class="flex flex-end pb-4 md:pb-0">
-                    <div class="nav-item">
+                    <div class="nav-item pr-4">
                         <NuxtLink to="/terms-of-service" active-class="active"
-                            class="px-4 py-2 flex items-center text-sm font-bold hover:opacity-75">
+                            class="flex items-center text-sm font-bold hover:opacity-75">
                             <strong>Terms of Service</strong>
                         </NuxtLink>
                     </div>
-                    <div class="nav-item">
+                    <div class="nav-item pl-4">
                         <NuxtLink to="/privacy-policy" active-class="active"
-                            class="px-4 py-2 flex items-center text-sm font-bold hover:opacity-75">
+                            class="flex items-center text-sm font-bold hover:opacity-75">
                             <strong>Privacy Policy</strong>
                         </NuxtLink>
                     </div>
