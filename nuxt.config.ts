e6d16de506963c052226bf326c57ff036771b156
pchae <20016424+pchae@users.noreply.github.com>
2023-02-26 17:15:47 -0500

    remove pinia alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -75,9 +75,4 @@ export default defineNuxtConfig({
         }
     },
 
-    // https://stackoverflow.com/questions/74003458/cannot-find-module-pinia-dist-pinia-mjs-when-using-run-dev
-    alias: {
-        'pinia': '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
-    },
-
 })
