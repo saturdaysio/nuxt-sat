d897e941e8c47e20f352ca53dafdd7fa9c51c5f9
pchae <20016424+pchae@users.noreply.github.com>
2023-03-17 03:13:08 -0400

    fix active-class placement

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,8 +2,8 @@
 	<footer class="bg-black">
 		<div class="mx-auto max-w-7xl overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
 			<nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
-				<div v-for="item in navigation.main" :key="item.name" class="nav-item pb-6" active-link="active">
-					<NuxtLink :to="item.to" class="text-base leading-6">{{ item.name }}</NuxtLink>
+				<div v-for="item in navigation.main" :key="item.name" class="nav-item pb-6">
+					<NuxtLink :to="item.to" class="text-base leading-6" active-class="active">{{ item.name }}</NuxtLink>
 				</div>
 			</nav>
 			<p class="mt-10 text-center text-md leading-5 text-gray-500">&copy; 2016 - 2023 Saturdays.io All rights reserved.</p>
