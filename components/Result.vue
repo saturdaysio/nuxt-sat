25a79baa022f28a84a14d09c0c58b89ef6cf8731
pchae <20016424+pchae@users.noreply.github.com>
2023-03-17 02:39:57 -0400

    update text style

--- components/Result.vue
+++ components/Result.vue
@@ -1,14 +1,14 @@
 <template>
 	<div class="bg-black-900 py-24 sm:py-32">
-		<div class="mx-auto max-w-8xl px-6 lg:px-8">
-			<div class="mx-auto max-w-2xl lg:max-w-none">
+		<div class="mx-auto max-w-8xl px-4 lg:px-8">
+			<div class="mx-auto max-w-7xl">
 				<div class="text-center">
-					<h2 class="text-3xl font-bold tracking-tight text-white sm:text-6xl">Our stats</h2>
+					<h2 class="text-3xl font-bold tracking-tight text-white sm:text-5xl">Our stats</h2>
 				</div>
 				<dl class="mt-16 grid grid-cols-1 gap-2 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-2">
 					<div v-for="item in resultStore.resultJson.result" :key="item.id" class="flex flex-col p-8">
-						<dd class="order-first text-8xl font-bold tracking-tight text-white">{{ item.title }}</dd>
-						<dt class="text-sm font-normal leading-6 text-gray-300">{{ item.desc }}</dt>
+						<dd class="order-first text-8xl font-thin tracking-tight text-grad-01">{{ item.title }}</dd>
+						<dt class="text-xl font-normal text-white">{{ item.desc }}</dt>
 					</div>
 				</dl>
 			</div>
