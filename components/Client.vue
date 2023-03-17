59143de00c82e90face4804aaeb49d654264e5f5
pchae <20016424+pchae@users.noreply.github.com>
2023-03-17 14:55:51 -0400

    update layout with tailwindui

--- components/Client.vue
+++ components/Client.vue
@@ -1,32 +1,33 @@
 <template>
-	<section id="clients" class="mx-auto px-4 py-16 bg-black-900">
-		<div class="container lg:max-w-5xl mx-auto my-8">
-			<div class="pb-8">
-				<h1 class="text-3xl lg:text-5xl text-center font-bold text-grad-01">We partner with brands brave
-					enough to do things differently.</h1>
-			</div>
-
-			<div id="grid" class="flex flex-wrap">
-				<div class="logo" v-for="item in clientStore.clientJson.clients" :key="item.id">
-					<figure>
-						<img class="w-36 h-36" :src="item.imgSrc" :alt="item.imgAlt" loading="lazy" :width="item.width"
-							:height="item.height">
-					</figure>
-				</div>
-			</div>
+	<div class="bg-black py-24 sm:py-32">
+	  <div class="mx-auto max-w-6xl px-4">
+		<div class="pb-8">
+			<h1 class="text-3xl lg:text-5xl text-center font-bold text-grad-01">We partner with brands brave enough to do things differently.</h1>
 		</div>
-	</section>
-</template>
+
+		<div id="grid" class=" grid grid-cols-2 gap-4 overflow-hidden sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
+		  <div class="logo" v-for="item in clientStore.clientJson.clients" :key="item.id">
+			<figure>
+				<img class="w-36 h-36" :src="item.imgSrc" :alt="item.imgAlt" loading="lazy" :width="item.width" :height="item.height">
+			</figure>
+		  </div>
+
+		</div>
+	  </div>
+	</div>
+  </template>
+
 
 <script lang="ts">
-import { useClientStore } from "~~/store/client";
 
-export default {
-	setup() {
-		const clientStore = useClientStore()
-		return { clientStore }
+	import { useClientStore } from "~~/store/client";
+
+	export default {
+		setup() {
+			const clientStore = useClientStore()
+			return { clientStore }
+		}
 	}
-}
 
 </script>
 
@@ -35,22 +36,17 @@ export default {
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		width: 25%;
 		flex-shrink: 0;
 		padding: 0 2rem;
 	}
 
 
 	@media only screen and (max-width: 800px) {
-		.logo {
-			width: 33.33%;
-		}
 
 	}
 
 	@media only screen and (max-width: 640px) {
 		.logo {
-			width: 50%;
 			padding: 0 1rem;
 		}
 	}
