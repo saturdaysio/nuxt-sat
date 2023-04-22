032a480647b4e533b29ed8e4cb1b3916453a43a6
pchae <20016424+pchae@users.noreply.github.com>
2023-04-21 21:02:30 -0400

    fix template format

--- components/Header.vue
+++ components/Header.vue
@@ -44,18 +44,9 @@
 		</header>
 
 		<div class="relative isolate overflow-hidden pt-14">
-			<img src="https://images.unsplash.com/photo-1498036882173-b41c28a8ba34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="City image" class="absolute inset-0 -z-10 h-full w-full object-cover" />
+
 			<div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
 				<!-- highlight top left -->
-				<svg class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678">
-					<path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".2" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
-					<defs>
-						<linearGradient id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
-							<stop stop-color="#9089FC" />
-							<stop offset="1" stop-color="#FF80B5" />
-						</linearGradient>
-					</defs>
-				</svg>
 			</div>
 			<!-- headline -->
 			<div class="mx-auto max-w-6xl py-32 sm:py-48 lg:py-56">
@@ -65,15 +56,7 @@
 			</div>
 			<!-- highlight bottom right -->
 			<div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-				<svg class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678">
-					<path fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)" fill-opacity=".2" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
-					<defs>
-						<linearGradient id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
-							<stop stop-color="#9089FC" />
-							<stop offset="1" stop-color="#FF80B5" />
-						</linearGradient>
-					</defs>
-				</svg>
+
 			</div>
 		</div>
 	</div>
--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -12,10 +12,8 @@
 				<div id="definition" class="py-8">
 					<p>
 						Saturdays.io Inc. (“our” “we” or “us”) takes your (“you” “your” or the “User”) privacy seriously. We
-						may
-						collect use and share some of your personal information. This Privacy Policy is a guide to explain
-						how we do
-						that and your rights as a User. This Privacy Policy forms part of our Terms of Service available at
+						may collect use and share some of your personal information. This Privacy Policy is a guide to explain
+						how we do that and your rights as a User. This Privacy Policy forms part of our Terms of Service available at
 						<NuxtLink to="/terms-of-service">https://saturdays.io/terms-of-service</NuxtLink>
 					</p>
 
@@ -27,8 +25,7 @@
 					</h1>
 					<p>
 						This Privacy Policy covers our treatment of your personally identifiable information and any other
-						information
-						you may provide us when using our applications websites content features products or other services
+						information you may provide us when using our applications websites content features products or other services
 						(referred to herein as the “Services”).
 					</p>
 				</div>
@@ -39,24 +36,18 @@
 					</h1>
 					<p class="pb-4">
 						We focus on using your personal information to provide and improve our Services respond to your
-						requests
-						service user accounts connected to our Services provide you with services or information you may
-						request
-						communicate with you and inform you about new products and services that may be of interest and to
-						improve
-						our site. We collect the following types of information:
+						requests service user accounts connected to our Services provide you with services or information you may
+						request communicate with you and inform you about new products and services that may be of interest and to
+						improve our site. We collect the following types of information:
 					</p>
 
 					<div class="prose max-w-none">
 						<ul class="list-disc">
 							<li>Information You Provide Us: Name mailing address phone number email address contact
-								preferences device
-								identifiers IP address location information credit card information and profile information
-								where the
-								contact is via social media.</li>
+								preferences device identifiers IP address location information credit card information and profile information
+								where the contact is via social media.</li>
 							<li>Information We Collect from Users of our Services: Name mailing address Phone number Email
-								address
-								Contact preferences Device identifiers IP address and location information.</li>
+								address Contact preferences Device identifiers IP address and location information.</li>
 							<li>Information We Collect from Third Parties: We don't collect information from third parties.
 							</li>
 						</ul>
@@ -81,18 +72,15 @@
 							<li>Measure and better understand the performance of our Services</li>
 							<li>Communicate with Users</li>
 							<li>Protect Users the community us and other legal reasons such as complying with applicable
-								laws assisting
-								with law enforcement agencies if legally mandated to find instances of abuse or misuse of
-								our services or
-								illegal activity maintain security and safety when using our Services</li>
+								laws assisting with law enforcement agencies if legally mandated to find instances of abuse or misuse of
+								our services or illegal activity maintain security and safety when using our Services</li>
 						</ul>
 					</div>
 
 
 					<p>
 						In the event that we determine we need to use your personal information for a reason not included in
-						this
-						Privacy Policy we will ask for your consent first.
+						this Privacy Policy we will ask for your consent first.
 					</p>
 				</div>
 
@@ -102,8 +90,7 @@
 					</h1>
 					<p class="pb-4">
 						We do not sell your personal information. To better provide Services we may share your personal
-						information
-						with third parties (like social networks) and our service providers.
+						information with third parties (like social networks) and our service providers.
 					</p>
 					<p class="pb-4">
 						We may share your information with third parties in other cases such as:
@@ -112,18 +99,13 @@
 					<div class="prose max-w-none">
 						<ul class="list-disc">
 							<li>If a government body requests information and we deemed it to be necessary to disclose such
-								information
-								in order to comply with applicable laws and legal processes;</li>
+								information in order to comply with applicable laws and legal processes;</li>
 							<li>To cooperate with law enforcement or other government authorities as it relates to any
-								investigations or
-								other inquiries to the extent we deem it necessary to protect either our users the public or
-								us.</li>
+								investigations or other inquiries to the extent we deem it necessary to protect either our users the public or us.</li>
 							<li>To defend or pursue legal claims as required or permitted by law.</li>
 							<li>Enforcing our Terms of Service.</li>
 							<li>Responding to any legal process including court orders and search warrants.</li>
-							<li>To detect or protect against fraud security technical or legal issues associated with the
-								use of our
-								Services.</li>
+							<li>To detect or protect against fraud security technical or legal issues associated with the use of our Services.</li>
 							<li>At any time that you consent to such sharing.</li>
 						</ul>
 					</div>
@@ -138,24 +120,18 @@
 					</p>
 					<p class="pb-4">
 						Our Services have been built in a way to minimize the amount of personal information that is stored
-						within
-						them. Nonetheless if you want to review update change or delete the information that you have shared
-						with
-						us including deletion of your account you may send a request to do so to <NuxtLink
+						within them. Nonetheless if you want to review update change or delete the information that you have shared
+						with us including deletion of your account you may send a request to do so to <NuxtLink
 							to="mailto:support@saturdays.io" rel="noreferrer" target="_blank">support [at] saturdays.io
 						</NuxtLink>
 					</p>
 					<p>
 						Even in the event that we delete your personal information we still may retain some information for
 						legitimate business purposes such as maintenance of security of the Services or to comply with
-						applicable
-						laws
-						or regulations depending on the circumstances of such deletion. You understand and agree that we may
-						have
-						responsibility to law enforcement or other governmental entities or to our stakeholders to maintain
+						applicable laws or regulations depending on the circumstances of such deletion. You understand and agree that we may
+						have responsibility to law enforcement or other governmental entities or to our stakeholders to maintain
 						information if there is the possibility of any unsafe or illegal behavior to ensure the Services are
-						not
-						exploited in the future.
+						not exploited in the future.
 					</p>
 				</div>
 
@@ -165,9 +141,7 @@
 					</h1>
 					<p>
 						We do not track our Users over time and across third party websites and therefore our Services do
-						not
-						respond
-						to Do Not Track signals.
+						not respond to Do Not Track signals.
 					</p>
 				</div>
 
@@ -177,8 +151,7 @@
 					</h1>
 					<p>
 						We do not collect “personal data” about or monitor behavior of “data subjects” as those terms are
-						defined in
-						GDPR Art. 4(1). We do not “target” individuals in the EEU.
+						defined in GDPR Art. 4(1). We do not “target” individuals in the EEU.
 					</p>
 				</div>
 
@@ -188,11 +161,8 @@
 					</h1>
 					<p>
 						We do not knowingly collect or solicit the personal information of anyone under the age of 13 even
-						with a
-						parent or guardian's consent. Our services are not intended for children and in the event that we
-						determine
-						that a person under 13 has provided personal information we will delete that information
-						immediately.
+						with a parent or guardian's consent. Our services are not intended for children and in the event that we
+						determine that a person under 13 has provided personal information we will delete that information immediately.
 					</p>
 				</div>
 
@@ -203,10 +173,8 @@
 					<p>
 						We may update this Privacy Policy from time to time as necessary based on changes to our company our
 						Services or to applicable laws. In the event that we do update this Privacy Policy such changes will
-						be made
-						online. You will be notified of any material changes through the Services or e-mail. Your use of the
-						Services
-						means that you are agreeing to the most recently updated Privacy Policy and Terms of Service.
+						be made online. You will be notified of any material changes through the Services or e-mail. Your use of the
+						Services means that you are agreeing to the most recently updated Privacy Policy and Terms of Service.
 					</p>
 				</div>
 
@@ -216,12 +184,9 @@
 					</h1>
 					<p>
 						All of our rights and obligations pursuant to this Privacy Policy are freely assignable by us to any
-						of our
-						affiliates in connection with a merger acquisition restructuring or sale of assets or by operation
-						of law
-						or otherwise and we may transfer your information to any of our affiliates successor entities or a
-						new
-						owner.
+						of our affiliates in connection with a merger acquisition restructuring or sale of assets or by operation
+						of law or otherwise and we may transfer your information to any of our affiliates successor entities or a
+						new owner.
 					</p>
 				</div>
 
@@ -240,7 +205,8 @@
 
 	<Footer />
 
-</div></template>
+	</div>
+</template>
 
 
 <script setup lang="ts">
