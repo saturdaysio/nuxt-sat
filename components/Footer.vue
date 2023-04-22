59482949f8a42fe7ec6da2f093e22af477cc5cfb
pchae <20016424+pchae@users.noreply.github.com>
2023-04-22 17:36:26 -0400

    add scss link override

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
 		<div class="mx-auto max-w-7xl overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
 			<nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
 				<div v-for="item in navigation.main" :key="item.name" class="nav-item pb-6">
-					<NuxtLink :to="item.to" class="text-base leading-6">{{ item.name }}</NuxtLink>
+					<NuxtLink :to="item.to" class="text-base leading-6 text-white">{{ item.name }}</NuxtLink>
 				</div>
 			</nav>
 			<p class="mt-10 text-center text-md leading-5 text-gray-500">&copy; 2016 - 2023 Saturdays.io All rights reserved.</p>
@@ -12,6 +12,7 @@
 </template>
   
 <script setup lang="ts">
+
 	import { defineComponent, h } from 'vue'
 
 	const navigation = {
@@ -22,8 +23,16 @@
 			{ name: 'Privacy Policy', to: '/privacy-policy' },
 		],
 	}
+
 </script>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+	a {
+		&:hover, &:focus {
+			text-decoration: none;
+  		}
+	}
+
 </style>
