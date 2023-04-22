9c259ebc954e81a381d7b6c323563da9e81af7a1
pchae <20016424+pchae@users.noreply.github.com>
2023-04-22 18:17:52 -0400

    remove lang parameter

--- app.vue
+++ app.vue
@@ -25,17 +25,17 @@
 </script>
 
 
-<style lang="scss">
+<style>
 
 	.page-enter-active,
 	.page-leave-active {
-		transition: all 0.3s;
+		transition: all 0.4s;
 	}
 
 	.page-enter-from,
 	.page-leave-to {
 		opacity: 0;
-		transition: all 0.3s;
+		transition: all 0.4s;
 	}
 
 </style>
