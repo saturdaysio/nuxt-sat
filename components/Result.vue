7d10649dd52d28a3a5d29f0f32adc0f318d1c7cf
pchae <20016424+pchae@users.noreply.github.com>
2023-04-24 13:06:18 -0400

    reduce font size and weight on stat title

--- components/Result.vue
+++ components/Result.vue
@@ -7,7 +7,7 @@
 				</div>
 				<dl class="mt-16 grid grid-cols-1 gap-2 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-2">
 					<div v-for="item in resultStore.resultJson.result" :key="item.id" class="flex flex-col p-8">
-						<dd class="order-first text-6xl md:text-8xl font-thin tracking-tight text-grad-01">{{ item.title }}</dd>
+						<dd class="order-first text-4xl md:text-6xl font-light tracking-tight text-grad-01">{{ item.title }}</dd>
 						<dt class="text-xl font-normal text-white">{{ item.desc }}</dt>
 					</div>
 				</dl>
