d6533a45bc936d9009417c220b14a96363125c14
pchae <20016424+pchae@users.noreply.github.com>
2023-06-03 15:46:10 -0400

    update background opacity

--- components/Result.vue
+++ components/Result.vue
@@ -6,7 +6,7 @@
 					<h2 class="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tight text-center text-white">Our stats</h2>
 				</div>
 				<dl class="mt-16 grid grid-cols-1 gap-4 overflow-hidden text-center sm:grid-cols-2 lg:grid-cols-2">
-					<div v-for="item in resultStore.resultJson.result" :key="item.id" class="flex flex-col p-8 bg-gray-600/20">
+					<div v-for="item in resultStore.resultJson.result" :key="item.id" class="flex flex-col p-8 bg-gray-600/10">
 						<dd class="order-first text-5xl md:text-6xl lg:text-8xl font-light tracking-tight text-grad-01">{{ item.title }}</dd>
 						<dt class="text-xl lg:text-2xl font-normal text-white">{{ item.desc }}</dt>
 					</div>
