1bb8b86371ef9e1604c6339c94c18e4571482775
pchae <20016424+pchae@users.noreply.github.com>
2023-06-03 14:21:45 -0400

    hide border on mobile logo

--- components/Nav.vue
+++ components/Nav.vue
@@ -10,10 +10,10 @@
 					</NuxtLink>
 				</div>
 				<div class="flex lg:hidden">
-					<button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400" @click="mobileMenuOpen = true">
-						<span class="sr-only">Open main menu</span>
-						<Bars3Icon class="h-6 w-6" aria-hidden="true" />
-					</button>
+						<button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400" @click="mobileMenuOpen = true">
+							<span class="sr-only">Open main menu</span>
+							<Bars3Icon class="h-6 w-6" aria-hidden="true" />
+						</button>
 				</div>
 				<div class="hidden lg:flex lg:flex-1 lg:gap-x-8 lg:justify-end nav-item">
 					<NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="text-md text-white" active-class="active">{{ item.name }}</NuxtLink>
@@ -25,14 +25,14 @@
 				<div class="fixed inset-0 z-50" />
 				<DialogPanel class="fixed inset-y-0 right-0 z-50 w-full h-full overflow-y-auto px-6 py-4 backdrop-blur bg-black/60">
 					<div class="flex items-center justify-between">
-						<NuxtLink to="/" class="">
+						<NuxtLink to="/" class="border-none focus:ring-0">
 							<span class="sr-only">Saturdays.io</span>
 							<img class="h-8 w-8" src="@/assets/img/logo.svg" alt="Saturdays.io logo" width="32px" height="32px" />
 						</NuxtLink>
-						<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-400" @click="mobileMenuOpen = false">
-							<span class="sr-only">Close menu</span>
-							<XMarkIcon class="h-6 w-6" aria-hidden="true" />
-						</button>
+							<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-400" @click="mobileMenuOpen = false">
+								<span class="sr-only">Close menu</span>
+								<XMarkIcon class="h-6 w-6" aria-hidden="true" />
+							</button>
 					</div>
 					<!-- Navigation links -->
 					<div class="my-6 flex h-4/6">
@@ -64,5 +64,31 @@
 </script>
 
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+	.fade-enter-active,
+	.fade-leave-active {
+		transition: opacity 0.3s ease;
+	}
+
+	.vfade-enter-from,
+	.fade-leave-to {
+		opacity: 0;
+		transition: opacity 0.3s ease;
+	}
+
+	.fade-leave-to,
+	.fade-enter-from {
+		opacity: 0;
+	}
+
+	.fade-leave-from,
+	.fade-enter-to {
+		opacity: 1;
+	}
+
+	.fade-leave-active,
+	.fade-enter-active {
+		transition: opacity 0.3s ease;
+	}
 </style>
