9d505cfda75f255676491d920c96eecd7642d0d0
pchae <20016424+pchae@users.noreply.github.com>
2023-06-03 15:46:23 -0400

    adjust responsive text sizes

--- pages/about.vue
+++ pages/about.vue
@@ -8,13 +8,13 @@
 		<section id="ethics" class="w-full mx:auto px-4 py-16 bg-black">
 			<div class="container lg:max-w-5xl mx-auto my-8">
 				<div class="pb-4">
-					<h1 class="text-3xl md:text-4xl lg:text-5xl text-purple-pink pb-8">Keeping it 100</h1>
-					<p class="text-xl text-white">We believe that our work and ethics
+					<h1 class="text-4xl md:text-5xl lg:text-6xl text-purple-pink pb-8	">Keeping it 100</h1>
+					<p class="text-xl md:text-2xl text-white pb-4">We believe that our work and ethics
 					define who we are. We're Human-centered Designers and Developers behind some of North America's
 					biggest mobile apps and digital brands looking to make a net-positive impact in the world.</p>
 				</div>
 				<div>
-				<p class="text-xl text-white">Our approach to building software prioritizes accessibility, user-centric design, and inclusion. We research and generate ideas, then
+				<p class="text-xl md:text-2xl text-white">Our approach to building software prioritizes accessibility, user-centric design, and inclusion. We research and generate ideas, then
 					build, test, and iterate on them using real-world analytics and actionable KPIs. The emphasis on data-driven design, diversity, user empathy, and sustainability aren't buzzwords, they're why we
 					win.</p>
 				</div>
