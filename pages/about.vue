c7fc39a053938e3a62a638e6761d81c168ca50c0
pchae <20016424+pchae@users.noreply.github.com>
2023-06-03 16:18:24 -0400

    update aspect ratio

--- pages/about.vue
+++ pages/about.vue
@@ -33,7 +33,7 @@
 				<div class="mx-auto max-w-7xl mt-8 px-6">
 					<ul role="list" class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4">
 						<li v-for="person in team" :key="person.name">
-							<img class="aspect-[14/13] w-full rounded-md object-cover" draggable="false" :src="person.imageUrl" alt="pfp" />
+							<img class="aspect-[1/1] w-full rounded-md object-cover" draggable="false" :src="person.imageUrl" alt="pfp" />
 							<h3 class="mt-2 text-xl md:text-2xl font-bold leading-8 text-white">{{ person.name }}</h3>
 							<p class="text-base md:text-lg text-gray-300">{{ person.role }}</p>
 							<p class="text-sm md:text-base text-gray-500">{{ person.location }}</p>
