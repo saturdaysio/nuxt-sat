2fa87471dfb8097e4eaf484c86de88b824d5c343
pchae <20016424+pchae@users.noreply.github.com>
2023-06-04 03:17:05 -0400

    update team grid and about text

--- pages/about.vue
+++ pages/about.vue
@@ -27,11 +27,11 @@
 			<div class="container lg:max-w-7xl mx-auto my-8">
 				<div class="mx-auto max-w-5xl pb-4">
 					<h1 class="text-4xl md:text-5xl lg:text-6xl text-center text-purple-pink pb-8">Who we are</h1>
-					<p class="text-xl md:text-2xl text-white">We're a globally distributed design-led team of technologists aligned with a common interest in improving life for all people. We operate like a sports team by continuously evolving our roster with specialists to meet the unique needs of each project.</p>
+					<p class="text-xl md:text-2xl text-white">We're a globally distributed group of technologists aligned with a common interest in improving life for all people. We operate like a sports team by continuously evolving our roster with specialists to meet the unique needs of each project.</p>
 				</div>
 
 				<div class="mx-auto max-w-7xl mt-8 px-6">
-					<ul role="list" class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4">
+					<ul role="list" class="mx-auto grid max-w-2xl grid-cols-2 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4">
 						<li v-for="person in team" :key="person.name">
 							<img class="aspect-[4/3] w-full rounded-md object-cover" :src="person.imageUrl" alt="pfp" draggable="false" loading="lazy" />
 							<h2 class="mt-2 text-xl md:text-2xl font-bold leading-8 text-white">{{ person.name }}</h2>
