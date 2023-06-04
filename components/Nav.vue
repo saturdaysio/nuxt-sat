f13b6af48b8622fa0152dff606420323c78ee737
pchae <20016424+pchae@users.noreply.github.com>
2023-06-04 03:10:57 -0400

    refactor component for transitions

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,69 +1,80 @@
 <template>
-	<nav class="relative">
+	<Disclosure as="nav" class="relative">
 		<div class="fixed w-full px-4 z-50">
 			<header class="absolute inset-x-0 top-0 backdrop-blur bg-black/80 border-b border-white/10">
-			<nav class="flex items-center justify-between max-w-7xl mx-auto px-6 py-4" aria-label="Global">
-				<div class="flex lg:flex-1">
-					<NuxtLink to="/" class="focus:ring-0 focus:ring-transparent">
-						<span class="sr-only">Saturdays.io</span>
-						<img class="h-8 w-8" src="@/assets/img/logo.svg" alt="Saturdays.io logo" width="32px" height="32px" />
-					</NuxtLink>
-				</div>
-				<div class="flex lg:hidden">
-						<button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400" @click="mobileMenuOpen = true">
-							<span class="sr-only">Open main menu</span>
-							<Bars3Icon class="h-6 w-6" aria-hidden="true" />
-						</button>
-				</div>
-				<div class="hidden lg:flex lg:flex-1 lg:gap-x-8 lg:justify-end nav-item">
-					<NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="text-md text-white" active-class="active">{{ item.name }}</NuxtLink>
-				</div>
-			</nav>
 
-			<!-- Mobile menu -->
-			<Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-				<div class="fixed inset-0 z-50" />
-				<DialogPanel class="fixed inset-y-0 right-0 z-50 w-full h-full overflow-y-auto px-6 py-4 backdrop-blur bg-black/60">
-					<div class="flex items-center justify-between">
-						<NuxtLink to="/" class="focus:outline-none">
-							<span class="sr-only">Saturdays.io</span>
-							<img class="h-8 w-8" src="@/assets/img/logo.svg" alt="Saturdays.io logo" width="32px" height="32px" />
-						</NuxtLink>
-						<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-400" @click="mobileMenuOpen = false">
-							<span class="sr-only">Close menu</span>
-							<XMarkIcon class="h-6 w-6" aria-hidden="true" />
-						</button>
-					</div>
-					<!-- Navigation links -->
-					<div class="my-6 flex h-4/6">
-						<div class="flex flex-col justify-center nav-item space-y-2 py-6">
-							<NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="-mx-3 block rounded-lg py-4 px-4 text-4xl font-black uppercase text-white" active-class="active">{{ item.name }}</NuxtLink>
+				<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+					<div class="flex h-16 items-center justify-between">
+						<div class="flex items-center">
+							<div class="flex-shrink-0">
+								<NuxtLink to="/">
+									<img class="block h-8 w-auto lg:hidden" src="@/assets/img/logo.svg" alt="Saturdays.io" />
+									<img class="hidden h-8 w-auto lg:block" src="@/assets/img/logo.svg" alt="Saturdays.io" />
+								</NuxtLink>
+							</div>
+						</div>
+
+						<div class="hidden sm:ml-6 sm:block">
+							<div class="flex items-center">
+								<div class="flex space-x-4">
+									<NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="px-3 py-2 text-md font-bold text-white">{{ item.name }}</NuxtLink>
+								</div>
+							</div>
+						</div>
+						<div class=" flex sm:hidden">
+							<!-- Mobile menu button -->
+							<DisclosureButton class="inline-flex items-center justify-center p-2 text-white focus:none">
+								<span class="sr-only">Open main menu</span>
+								<Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+								<XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+							</DisclosureButton>
 						</div>
 					</div>
-				</DialogPanel>
-			</Dialog>
-		</header>
+				</div>
+
+				<DisclosurePanel class="sm:hidden">
+					<div class=" right-0 z-50 w-full h-full overflow-y-auto space-y-1 px-6 py-4 backdrop-blur bg-black/60">
+						<NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="block px-3 py-2 text-2xl font-bold text-white">{{ item.name }}</NuxtLink>
+					</div>
+				</DisclosurePanel>
+
+			</header>
 		</div>
-	</nav>
+
+	</Disclosure>
 </template>
 
 
 <script setup lang="ts">
 
-	import { ref } from 'vue'
-	import { TransitionRoot, Dialog, DialogPanel } from '@headlessui/vue'
-	import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
-	const navigation = [
-		{ name: 'Work', to: '/' },
-		{ name: 'About', to: '/about' },
-	]
-
-	const mobileMenuOpen = ref(false)
+const navigation = [
+	{ name: 'Work', to: '/' },
+	{ name: 'About', to: '/about' },
+]
 
 </script>
 
 
 <style lang="scss">
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 0.5s ease;
+}
 
-</style>
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}</style>
