3f7edb6656a378e2b5bc9eb8db2c5666e05d2558
pchae <20016424+pchae@users.noreply.github.com>
2023-06-19 21:44:42 -0400

    fix transition tags

--- components/Nav2.vue
+++ components/Nav2.vue
@@ -12,7 +12,7 @@
                     </div>
                     <div class="flex lg:hidden">
                         <!-- Toggle -->
-                        <button type="button" class="-m-2.5 inline-flex items-center justify-center p-2.5 text-gray-400" @click="mobileMenuOpen = true">
+                        <button type="button" class="-m-2.5 inline-flex items-center justify-center p-2.5 text-gray-400" @click="mobileMenuOpen = !mobileMenuOpen">
                             <span class="sr-only">Open menu</span>
                             <Bars3Icon class="h-8 w-8" aria-hidden="true" />
                         </button>
@@ -23,34 +23,44 @@
                         <NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="text-md text-white" active-class="active">{{ item.name }}</NuxtLink>
                     </div>
                 </div>
-
+                <TransitionRoot :show="mobileMenuOpen">
                 <!-- Mobile menu -->
-                <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-                    <div v-if="mobileMenuOpen" class="fixed inset-0 z-50">
-                        <DialogPanel class="w-full h-full overflow-y-auto px-6 py-4 backdrop-blur bg-black/60">
-                            <div class="flex items-center justify-between">
-                                <!-- Logo -->
-                                <NuxtLink to="/" class="focus:outline-none">
-                                    <span class="sr-only">Saturdays.io</span>
-                                    <img class="h-8 w-8" src="@/assets/img/logo.svg" alt="Saturdays.io logo" width="32px" height="32px" />
-                                </NuxtLink>
+                    <Dialog as="div" class="lg:hidden" :open="mobileMenuOpen" @close="mobileMenuOpen = true">
+                        <TransitionChild
+                            enter="transition-opacity ease-in-out duration-200"
+                            enter-from="opacity-0"
+                            enter-to="opacity-100"
+                            leave="transition-opacity ease-in-out duration-200"
+                            leave-from="opacity-0"
+                            leave-to="opacity-100"
+                        >
+                            <div class="fixed inset-0 z-50">
+                                <DialogPanel class="w-full h-full overflow-y-auto px-6 py-4 backdrop-blur bg-black/80">
+                                    <div class="flex items-center justify-between">
+                                        <!-- Logo -->
+                                        <NuxtLink to="/" class="focus:outline-none">
+                                            <span class="sr-only">Saturdays.io</span>
+                                            <img class="h-8 w-8" src="@/assets/img/logo.svg" alt="Saturdays.io logo" width="32px" height="32px" />
+                                        </NuxtLink>
 
-                                <!-- Toggle -->
-                                <button type="button" class="-m-2.5 p-2.5 text-gray-400" @click="mobileMenuOpen = false">
-                                    <span class="sr-only">Close menu</span>
-                                    <XMarkIcon class="h-8 w-8" aria-hidden="true" />
-                                </button>
-                            </div>
+                                        <!-- Toggle -->
+                                        <button type="button" class="-m-2.5 inline-flex items-center justify-center p-2.5 text-gray-400" @click="mobileMenuOpen = !mobileMenuOpen">
+                                            <span class="sr-only">Close menu</span>
+                                            <XMarkIcon class="h-8 w-8" aria-hidden="true" />
+                                        </button>
+                                    </div>
 
-                            <!-- Navigation links -->
-                            <div class="flex h-4/5">
-                                <div class="flex flex-col justify-center nav-item space-y-2">
-                                    <NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="mx-4 block py-4 px-4 text-3xl sm:text-4xl md:text-5xl font-black text-white" active-class="active">{{ item.name }}</NuxtLink>
-                                </div>
+                                    <!-- Navigation links -->
+                                    <div class="flex h-4/5">
+                                        <div class="flex flex-col justify-center nav-item space-y-2">
+                                            <NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="mx-4 block py-4 px-4 text-3xl sm:text-4xl md:text-5xl font-black text-white" active-class="active">{{ item.name }}</NuxtLink>
+                                        </div>
+                                    </div>
+                                </DialogPanel>
                             </div>
-                        </DialogPanel>
-                    </div>
-                </Dialog>
+                        </TransitionChild>
+                    </Dialog>
+                </TransitionRoot>
             </header>
 		</div>
 	</nav>
@@ -60,7 +70,7 @@
 <script setup lang="ts">
 
 	import { ref } from 'vue'
-	import { Disclosure, DisclosureButton, DisclosurePanel, Dialog, DialogPanel } from '@headlessui/vue'
+	import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
 	import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 	const navigation = [
@@ -77,7 +87,7 @@
 
     .v-enter-active,
     .v-leave-active {
-    transition: opacity 0.5s ease;
+    transition: opacity 0.2s ease;
     }
 
     .v-enter-from,
