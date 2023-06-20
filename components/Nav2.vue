868d5bc7c27ef29e2f973933e02c3e173d519586
pchae <20016424+pchae@users.noreply.github.com>
2023-06-20 11:29:40 -0400

    fix indent

--- components/Nav2.vue
+++ components/Nav2.vue
@@ -27,30 +27,30 @@
                 <!-- Mobile menu -->
                     <Dialog as="div" class="lg:hidden" :open="mobileMenuOpen" @close="mobileMenuOpen = true">
 
-                            <div class="fixed inset-0 z-50">
-                                <DialogPanel class="w-full h-full overflow-y-auto px-6 py-4 backdrop-blur bg-black/80">
-                                    <div class="flex items-center justify-between">
-                                        <!-- Logo -->
-                                        <NuxtLink to="/" class="focus:outline-none">
-                                            <span class="sr-only">Saturdays.io</span>
-                                            <img class="h-8 w-8" src="@/assets/img/logo.svg" alt="Saturdays.io logo" width="32px" height="32px" />
-                                        </NuxtLink>
+                        <div class="fixed inset-0 z-50">
+                            <DialogPanel class="w-full h-full overflow-y-auto px-6 py-4 backdrop-blur bg-black/80">
+                                <div class="flex items-center justify-between">
+                                    <!-- Logo -->
+                                    <NuxtLink to="/" class="focus:outline-none">
+                                        <span class="sr-only">Saturdays.io</span>
+                                        <img class="h-8 w-8" src="@/assets/img/logo.svg" alt="Saturdays.io logo" width="32px" height="32px" />
+                                    </NuxtLink>
 
-                                        <!-- Toggle -->
-                                        <button type="button" class="-m-2.5 inline-flex items-center justify-center p-2.5 text-gray-400" @click="mobileMenuOpen = !mobileMenuOpen">
-                                            <span class="sr-only">Close menu</span>
-                                            <XMarkIcon class="h-8 w-8" aria-hidden="true" />
-                                        </button>
-                                    </div>
+                                    <!-- Toggle -->
+                                    <button type="button" class="-m-2.5 inline-flex items-center justify-center p-2.5 text-gray-400" @click="mobileMenuOpen = !mobileMenuOpen">
+                                        <span class="sr-only">Close menu</span>
+                                        <XMarkIcon class="h-8 w-8" aria-hidden="true" />
+                                    </button>
+                                </div>
 
-                                    <!-- Navigation links -->
-                                    <div class="flex h-4/5">
-                                        <div class="flex flex-col justify-center nav-item space-y-2">
-                                            <NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="mx-4 block py-4 px-4 text-3xl sm:text-4xl md:text-5xl font-black text-white" active-class="active">{{ item.name }}</NuxtLink>
-                                        </div>
+                                <!-- Navigation links -->
+                                <div class="flex h-4/5">
+                                    <div class="flex flex-col justify-center nav-item space-y-2">
+                                        <NuxtLink v-for="item in navigation" :key="item.name" :to="item.to" class="mx-4 block py-4 px-4 text-3xl sm:text-4xl md:text-5xl font-black text-white" active-class="active">{{ item.name }}</NuxtLink>
                                     </div>
-                                </DialogPanel>
-                            </div>
+                                </div>
+                            </DialogPanel>
+                        </div>
 
                     </Dialog>
 
