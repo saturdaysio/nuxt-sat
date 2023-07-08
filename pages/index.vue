7a850f23a3ba7a143f69677df1aae775083e9aef
pchae <20016424+pchae@users.noreply.github.com>
2023-07-08 11:37:19 -0400

    clean up width and margin classes

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,8 @@
 
 		<Header intro="Saturdays is a Canadian digital studio based in Toronto" />
 
-		<section class="w-full mx-auto px-4 py-16">
-			<div class="container lg:max-w-6xl mx-auto my-8">
+		<section class="px-4 py-16">
+			<div class=" max-w-6xl mx-auto">
 				<h1 class="text-3xl md:text-4xl lg:text-5xl text-center text-purple-pink">
 					We're a Data-driven digital studio that focuses on the Design, Development, and Strategy of digital products.
 				</h1>
@@ -14,14 +14,14 @@
 		</section>
 
 		
-		<section id="peek" class="w-full mx-auto px-4 py-16">
-			<div class="container mx-auto mt-8">
-				<div class="pb-8">
+		<section id="peek" class="px-4 py-16">
+			<div class="">
+				<div class="mt-8 pb-8">
 					<h1 class="text-4xl md:text-5xl lg:text-6xl text-center text-purple-pink">
 						What we're making
 					</h1>
 				</div>
-				<div class="lg:max-w-7xl mx-auto">
+				<div class="max-w-7xl mx-auto">
 					<img src="@/assets/img/mobile.webp" width="1600" height="1000" alt="mobile app interface examples" preload loading="lazy" draggable="false" />
 				</div>
 			</div>
