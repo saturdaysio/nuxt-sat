883d9647ebf9a09bc92b5b4b7ea6a20c3ded2bb3
pchae <20016424+pchae@users.noreply.github.com>
2023-07-08 11:37:41 -0400

    clean up padding classes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
 	<footer>
-		<div class="mx-auto max-w-7xl overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
+		<div class="max-w-7xl mx-auto overflow-hidden px-6 lg:px-8 py-20 sm:py-24">
 			<nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
 				<div v-for="item in navigation.main" :key="item.name" class="nav-item pb-6">
 					<NuxtLink :to="item.to" class="text-base leading-6 text-white">{{ item.name }}</NuxtLink>
 				</div>
 			</nav>
-			<p class="mt-10 sm:text-center text-sm md:text-base leading-5 text-light text-gray-500">&copy; 2016-2023 Saturdays.io All rights reserved.</p>
+			<p class="mt-10 text-sm md:text-base sm:text-center leading-5 text-light text-gray-400">&copy; 2016-2023 Saturdays.io All rights reserved.</p>
 		</div>
 	</footer>
 </template>
