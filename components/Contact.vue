fbd776e468b00b466bd9dd7f524994b1496e8d4a
pchae <20016424+pchae@users.noreply.github.com>
2023-07-22 20:02:57 -0400

    replace button prop

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,9 +1,9 @@
 <template>
 	<section id="cta" class="px-4 sm:px-6 lg:px-8 py-24 sm:py-32">
-		<div class="max-w-4xl mx-auto">
-			<h2 class="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tight text-center text-white">Have an interesting project?</h2>
+		<div class="max-w-7xl mx-auto px-4 py-20 bg-gray-800/20">
+			<h2 class="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tight text-center text-white">Contact us</h2>
 			<div class="mt-8 flex items-center justify-center gap-x-6">
-				<Button :cta="'Say Hello'" class="button primary" :to="'mailto:hello@saturdays.io'" rel="noreferrer" target="_blank" />
+				<Button :buttonLabel="'Say Hello'" class="button primary" :to="'mailto:hello@saturdays.io'" rel="noreferrer" target="_blank" />
 			</div>
 		</div>
 	</section>
