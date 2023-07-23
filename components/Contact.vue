2d1d80b51a981891c0caea70e940091703a01262
pchae <20016424+pchae@users.noreply.github.com>
2023-07-23 09:42:15 -0400

    change style to cta

--- components/Contact.vue
+++ components/Contact.vue
@@ -3,7 +3,7 @@
 		<div class="max-w-7xl mx-auto px-4 py-20 bg-gray-800/10">
 			<h2 class="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tight text-center text-white">Contact us</h2>
 			<div class="mt-8 flex items-center justify-center gap-x-6">
-				<Button :buttonLabel="'Say Hello'" class="button primary" :to="'mailto:hello@saturdays.io'" rel="noreferrer" target="_blank" />
+				<Button :buttonLabel="'Say Hello'" class="button cta" :to="'mailto:hello@saturdays.io'" rel="noreferrer" target="_blank" />
 			</div>
 		</div>
 	</section>
