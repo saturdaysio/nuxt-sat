b75962e9eca9947d51248dfb205c16da54d4e24d
pchae <20016424+pchae@users.noreply.github.com>
2023-07-22 20:03:11 -0400

    reduce padding top

--- components/Client.vue
+++ components/Client.vue
@@ -1,5 +1,5 @@
 <template>
-	<section class="px-4 py-24 sm:py-32">
+	<section class="px-4 py-16">
 	  <div class="max-w-6xl mx-auto">
 		<div class="px-4 pb-8">
 			<h1 class="text-3xl md:text-4xl lg:text-5xl text-center font-bold lime-to-aqua">We partner with brands that care about meaningful digital experiences</h1>
