053afad2403b613172b19a6158032834028e78fd
pchae <20016424+pchae@users.noreply.github.com>
2023-07-25 03:55:52 -0400

    add nitro fix for javascript heap out of memory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,6 +60,12 @@ export default defineNuxtConfig({
     },
 
 
+    nitro: {
+        prerender: {
+            concurrency: 1
+        }
+    },
+
     // Hybrid Rendering setup
     // https://nuxt.com/docs/guide/concepts/rendering
     routeRules: {
