70f39ad6997b1838d27f467f835658de66e92f77
pchae <20016424+pchae@users.noreply.github.com>
2023-07-26 22:19:11 -0400

    update template layout

--- components/Result.vue
+++ components/Result.vue
@@ -1,13 +1,13 @@
 <template>
 	<section id="stats" class="px-4 lg:px-8 py-16 md:py-20 lg:py-24">
-		<div class="max-w-6xl mx-auto">
+		<div class="max-w-7xl mx-auto">
 			<div>
 				<h2 class="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight text-center text-white">Our stats</h2>
 			</div>
-			<dl class="mt-16 grid grid-cols-1 gap-8 overflow-hidden sm:grid-cols-2 lg:grid-cols-2">
-				<div v-for="item in resultStore.resultJson.result" :key="item.id" class="flex flex-col p-8 rounded-lg bg-gray-800/20">
-					<dd class="order-first text-5xl md:text-6xl lg:text-8xl font-semibold tracking-tight lime-to-aqua">{{ item.title }}</dd>
-					<dt class="text-xl lg:text-2xl font-normal text-white">{{ item.desc }}</dt>
+			<dl class="mt-16 grid grid-cols-1 gap-6 overflow-hidden sm:grid-cols-2 xl:grid-cols-4">
+				<div v-for="item in resultStore.resultJson.result" :key="item.id" class="flex flex-col p-4 lg:p-8 rounded-lg bg-gray-800/20">
+					<dd class="order-first text-2xl md:text-3xl lg:text-3xl font-semibold tracking-tight lime-to-aqua mb-8 md:mb-12 lg:mb-16">{{ item.title }}</dd>
+					<dt class="text-base mb:text-lg lg:text-xl font-normal text-white">{{ item.desc }}</dt>
 				</div>
 			</dl>
 		</div>
