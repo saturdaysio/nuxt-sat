ff34f304b63bb4770d8c24c47081cb784b9f3500
pchae <20016424+pchae@users.noreply.github.com>
2023-07-29 16:49:14 -0400

    add route guard function

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,9 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
     const user = useSupabaseUser()
-    if (!user.value)
-        return navigateTo('/signin')
+
+    if (!user.value && to.path === '/profile') {
+        return navigateTo('/')
+    } else if (user.value && to.path === '/') {
+        return navigateTo('/profile')
+    }
 })
