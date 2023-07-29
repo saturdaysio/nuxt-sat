ccd9714b36de8a6989469d34a05ad6b4bc138af5
pchae <20016424+pchae@users.noreply.github.com>
2023-07-28 21:39:11 -0400

    refactor video tag and add muted for refresh fix

--- components/Ideals.vue
+++ components/Ideals.vue
@@ -12,7 +12,9 @@
 					<div class="mx-auto grid max-w-7xl grid-cols-1 md:grid-cols-1 gap-x-2 md:gap-x-4 lg:gap-x-6 gap-y-16 md:gap-y-24 lg:gap-y-32">
 						<div v-for="item in ideal" :key="item.title" class="p-2 gap-x-8 md:flex items-center even:flex-row-reverse md:even:text-right">
 							<div id="clip" class="md:w-6/12 mb-2">
-								<video class="w-full rounded object-fill" :src="item.video" autoplay playsinline loop />
+								<video class="w-full rounded object-fill" autoplay muted playsinline loop>
+									<source :src="item.video"  type="video/mp4">
+								</video>
 							</div>
 							<div id="meta" class="md:w-6/12 justify-center items-center">
 								<h2 class="mt-2 text-2xl sm:text-3xl lg:text-4xl font-bold leading-8 lime-to-aqua">{{ item.title }}</h2>
@@ -48,11 +50,6 @@
 		},
 		{
 			video: "/meme/awkwafina.mp4",
-			title: "Always learning",
-			desc: "This is a special time for startups and in history. We have unlimited creativity, ingenuity, and resources. We're making these years the most rewarding of our lives."
-		},
-		{
-			video: "/meme/vince-carter.mp4",
 			title: "No rockstars",
 			desc: "You won't find any 10Xers here, and nobody has time for egos. Check yourself before you wreck yourself."
 		},
