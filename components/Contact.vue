63542d9b589fd2c68663f10b6782b6bf529da773
pchae <20016424+pchae@users.noreply.github.com>
2023-07-30 12:22:50 -0400

    adjust bg colors

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,7 +2,7 @@
 	<section class="px-4 md:px-6 lg:px-8 py-16 md:py-24 lg:py-32">
 
 		<div class="mx-auto max-w-7xl">
-			<div class="relative isolate overflow-hidden bg-gray-900/60 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+			<div class="relative isolate overflow-hidden bg-gray-900/80 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
 				<div class="mx-auto max-w-4xl">
 					<h2 class="text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-bold tracking-tight text-white">Want to get in touch?</h2>
 				</div>
