08e1b479e045a3f0bfae8cdc955ca815f755e165
pchae <20016424+pchae@users.noreply.github.com>
2023-07-29 21:48:46 -0400

    update route name and add extra text size

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
 	<footer class="w-full border-t border-white/10">
 		<div class="max-w-7xl mx-auto overflow-hidden px-6 lg:px-8 py-20 sm:py-24">
-			<nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+			<nav class="-mb-6 columns-2 md:flex md:justify-center md:space-x-12" aria-label="Footer">
 				<div v-for="item in navigation.main" :key="item.name" class="nav-item pb-6">
-					<NuxtLink :to="item.to" class="text-base leading-6 text-white" active-class="active">{{ item.name }}</NuxtLink>
+					<NuxtLink :to="item.to" class="text-md sm:text-base leading-6 text-white" active-class="active">{{ item.name }}</NuxtLink>
 				</div>
 			</nav>
 
@@ -18,7 +18,7 @@
 		main: [
 			{ name: 'Work', to: '/' },
 			{ name: 'About', to: '/about' },
-			{ name: 'Client Sign in', to: '/signin' },
+			{ name: 'Client login', to: '/signin' },
 			{ name: 'Terms of Service', to: '/terms-of-service' },
 			{ name: 'Privacy Policy', to: '/privacy-policy' },
 		],
