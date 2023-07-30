6fb86ea76a9c27f47d750e53e8dc181c46786d3d
pchae <20016424+pchae@users.noreply.github.com>
2023-07-30 16:23:26 -0400

    update profile layout

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,35 +1,215 @@
 <template>
 	<div>
-
 		<Nav />
 
-			<section id="profile" class="px-4 sm:px-8 md:px-12 py-16 md:py-20 lg:py-24">
-				<div class="max-w-5xl mx-auto">
-					<div v-if="user" class="pb-4">
-						<h1 class="text-4xl md:text-5xl lg:text-6xl font-bold lime-to-aqua pb-8">User profile</h1>
-						<p class="text-md text-white">{{  user }}</p>
-						<div class="w-16 h-16 rounded-full mt-12 mb-4 bg-blue-400">
-							<h2 class="text-xl font-bold text-center text-white">PC</h2>
+		<div id="main">
+			<section class="mx-auto max-w-7xl pt-16">
+				<form class="mt-12 py-8">
+					<div class="space-y-12">
+						<div class="max-w-5xl mx-auto border-b border-white/10 pb-12">
+							<h2 class="text-4xl font-semibold leading-10 text-white">Profile</h2>
+							<p class="mt-1 text-base leading-6 text-gray-400">This information will be displayed publicly so be careful what you share.</p>
+
+							<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+								<div class="sm:col-span-4">
+									<label for="username"
+										class="block text-sm font-medium leading-6 text-white">Username</label>
+									<div class="mt-2">
+										<div class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+											<span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">saturdays.io/</span>
+											<input type="text"
+												name="username"
+												id="username"
+												autocomplete="username"
+												class="flex-1 border-0 bg-white/5 py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6"
+												placeholder="janesmith" />
+										</div>
+									</div>
+								</div>
+
+
+								<div class="col-span-full">
+									<label for="photo" class="block text-sm font-medium leading-6 text-white">Photo</label>
+									<div class="mt-2 flex items-center gap-x-3">
+										<UserCircleIcon class="h-12 w-12 text-gray-500" aria-hidden="true" />
+										<button type="button"
+											class="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">Change</button>
+									</div>
+								</div>
+
+								<div class="col-span-full">
+									<label for="cover-photo" class="block text-sm font-medium leading-6 text-white">Cover
+										photo</label>
+									<div
+										class="mt-2 flex justify-center rounded-lg border border-dashed border-white/25 px-6 py-10">
+										<div class="text-center">
+											<PhotoIcon class="mx-auto h-12 w-12 text-gray-500" aria-hidden="true" />
+											<div class="mt-4 flex text-sm leading-6 text-gray-400">
+												<label for="file-upload"
+													class="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-indigo-500">
+													<span>Upload a file</span>
+													<input id="file-upload" name="file-upload" type="file"
+														class="sr-only" />
+												</label>
+												<p class="pl-1">or drag and drop</p>
+											</div>
+											<p class="text-xs leading-5 text-gray-400">PNG, JPG, GIF up to 10MB</p>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
-						<p class="text-base md:text-base font-bold text-white pb-4">ID: </p>
-						<p class="text-base md:text-base font-bold text-white pb-4">Email: {{ user.identities[0].identity_data.email }}</p>
-						<p class="text-base md:text-base font-bold text-white pb-4">First name: {{  }}</p>
-						<p class="text-base md:text-base font-bold text-white pb-4">Last name: {{  }}</p>
-						<p class="text-base md:text-base font-bold text-white pb-4">Phone: {{  }}</p>
-						<p class="text-base md:text-base font-bold text-white pb-4">Created at: {{  }}</p>
-						<p class="text-base md:text-base font-bold text-white pb-4">Last update at: {{ }}</p>
+
+						<div v-if="user" class="max-w-5xl mx-auto border-b border-white/10 pb-12">
+							<h2 class="text-2xl font-bold leading-8 text-white">Personal Information</h2>
+							<p class="mt-1 text-base leading-6 text-gray-400">Use a permanent address where you can receive
+								mail.</p>
+
+							<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+								<div class="sm:col-span-3">
+									<label for="first-name" class="block text-sm font-medium leading-6 text-white">First
+										name</label>
+									<div class="mt-2">
+										<input type="text" name="first-name" id="first-name" autocomplete="given-name"
+											class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
+									</div>
+								</div>
+
+								<div class="sm:col-span-3">
+									<label for="last-name" class="block text-sm font-medium leading-6 text-white">Last
+										name</label>
+									<div class="mt-2">
+										<input type="text" name="last-name" id="last-name" autocomplete="family-name"
+											class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
+									</div>
+								</div>
+
+								<div class="sm:col-span-4">
+									<label for="email" class="block text-sm font-medium leading-6 text-white">Email
+										address</label>
+									<div class="mt-2">
+										<input id="email" name="email" type="email" autocomplete="email"
+											class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
+										{{ user.identities[0].identity_data.email }}
+									</div>
+								</div>
+
+								<div class="sm:col-span-3">
+									<label for="country"
+										class="block text-sm font-medium leading-6 text-white">Country</label>
+									<div class="mt-2">
+										<select id="country" name="country" autocomplete="country-name"
+											class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black">
+											<option>United States</option>
+											<option>Canada</option>
+											<option>Mexico</option>
+										</select>
+									</div>
+								</div>
+
+								<div class="col-span-full">
+									<label for="street-address"
+										class="block text-sm font-medium leading-6 text-white">Street address</label>
+									<div class="mt-2">
+										<input type="text" name="street-address" id="street-address"
+											autocomplete="street-address"
+											class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
+									</div>
+								</div>
+
+								<div class="sm:col-span-2 sm:col-start-1">
+									<label for="city" class="block text-sm font-medium leading-6 text-white">City</label>
+									<div class="mt-2">
+										<input type="text" name="city" id="city" autocomplete="address-level2"
+											class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
+									</div>
+								</div>
+
+								<div class="sm:col-span-2">
+									<label for="region" class="block text-sm font-medium leading-6 text-white">State /
+										Province</label>
+									<div class="mt-2">
+										<input type="text" name="region" id="region" autocomplete="address-level1"
+											class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
+									</div>
+								</div>
+
+								<div class="sm:col-span-2">
+									<label for="postal-code" class="block text-sm font-medium leading-6 text-white">ZIP /
+										Postal code</label>
+									<div class="mt-2">
+										<input type="text" name="postal-code" id="postal-code" autocomplete="postal-code"
+											class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="max-w-5xl mx-auto border-b border-white/10 pb-12">
+							<h2 class="text-2xl font-bold leading-8 text-white">Permissions</h2>
+							<p class="mt-1 text-base leading-6 text-gray-400">We'll always let you know about important changes, but you pick what else you want to hear about.</p>
+
+							<div class="mt-10 space-y-10">
+								<fieldset>
+									<legend class="text-sm font-semibold leading-6 text-white">By Email</legend>
+									<div class="mt-6 space-y-6">
+										<div class="relative flex gap-x-3">
+											<div class="flex h-6 items-center">
+												<input id="comments" name="comments" type="checkbox"
+													class="h-4 w-4 rounded border-white/10 bg-white/5 text-indigo-600 focus:ring-indigo-600 focus:ring-offset-gray-900" />
+											</div>
+											<div class="text-sm leading-6">
+												<label for="comments" class="font-medium text-white">Comments</label>
+												<p class="text-gray-400">Get notified when someones posts a comment on a
+													posting.</p>
+											</div>
+										</div>
+										<div class="relative flex gap-x-3">
+											<div class="flex h-6 items-center">
+												<input id="candidates" name="candidates" type="checkbox"
+													class="h-4 w-4 rounded border-white/10 bg-white/5 text-indigo-600 focus:ring-indigo-600 focus:ring-offset-gray-900" />
+											</div>
+											<div class="text-sm leading-6">
+												<label for="candidates" class="font-medium text-white">Candidates</label>
+												<p class="text-gray-400">Get notified when a candidate applies for a job.
+												</p>
+											</div>
+										</div>
+										<div class="relative flex gap-x-3">
+											<div class="flex h-6 items-center">
+												<input id="offers" name="offers" type="checkbox"
+													class="h-4 w-4 rounded border-white/10 bg-white/5 text-indigo-600 focus:ring-indigo-600 focus:ring-offset-gray-900" />
+											</div>
+											<div class="text-sm leading-6">
+												<label for="offers" class="font-medium text-white">Offers</label>
+												<p class="text-gray-400">Get notified when a candidate accepts or rejects an
+													offer.</p>
+											</div>
+										</div>
+									</div>
+								</fieldset>
+
+							</div>
+						</div>
 					</div>
-					<button @click="logout" class="primary">Sign out</button>
-				</div>
+
+					<div class="mt-6 flex items-center justify-end gap-x-6">
+						<button @click="logout" class="secondary">Sign out</button>
+						<button type="button" class="rounded-md border-2 border-green-500 px-6 py-4 text-base font-bold leading-6 text-white">Cancel</button>
+						<button type="submit" class="rounded-md bg-green-500 px-6 py-4 text-base font-bold text-black shadow-sm hover:bg-green-200">Save</button>
+					</div>
+				</form>
 			</section>
 
-		<Footer />
+			<Footer />
 
+		</div>
 	</div>
 </template>
 
-
 <script setup lang="ts">
+
+	import { PhotoIcon, UserCircleIcon } from '@heroicons/vue/24/solid'
 
 	definePageMeta({
 		middleware: ['auth']
@@ -48,7 +228,7 @@
 
 	const loading = ref(false)
 
-	
+
 	const logout = async () => {
 		let { error } = await supabase.auth.signOut();
 		navigateTo('/signin');
