81848eccfc5cf7962bc2f7a66a33c2d296d081f4
pchae <20016424+pchae@users.noreply.github.com>
2023-07-30 11:11:38 -0400

    change cta

--- components/Contact.vue
+++ components/Contact.vue
@@ -4,8 +4,7 @@
 		<div class="mx-auto max-w-7xl">
 			<div class="relative isolate overflow-hidden bg-gray-800/20 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
 				<div class="mx-auto max-w-4xl">
-					<h2 class="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tight text-white sm:text-4xl">We love collaborating with passionate people and brands</h2>
-					<p class="text-lg leading-8 text-gray-400">Not looking to partner, but still want to get in touch? We’d love to hear from you.</p>
+					<h2 class="text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-bold tracking-tight text-white">Want to get in touch?</h2>
 				</div>
 				<div class="mt-10 flex items-center justify-center gap-x-6">
 					<Button :buttonLabel="'Say hello'" class="primary" :to="'mailto:hello@saturdays.io'" rel="noreferrer" target="_blank" />
