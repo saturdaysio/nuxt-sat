5801d9b192a2766dbacee94aa73ba551be1cbafc
pchae <20016424+pchae@users.noreply.github.com>
2023-07-30 12:57:24 -0400

    add suspense component

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,9 @@
 		<div class="max-w-7xl mx-auto overflow-hidden px-6 lg:px-8 py-20 sm:py-24">
 			<nav class="-mb-6 columns-2 items-start md:flex md:justify-center md:space-x-12" aria-label="Footer">
 				<div v-for="item in navigation.main" :key="item.name" class="nav-item pb-6">
-					<NuxtLink :to="item.to" class="text-md sm:text-base leading-6 text-white" active-class="active">{{ item.name }}</NuxtLink>
+					<suspense suspensible>
+						<NuxtLink :to="item.to" class="text-md sm:text-base leading-6 text-white" active-class="active">{{ item.name }}</NuxtLink>
+					</suspense>
 				</div>
 			</nav>
 
