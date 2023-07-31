4838e9d543b5a6504e73a12dd31a3be133a85ebd
pchae <20016424+pchae@users.noreply.github.com>
2023-07-30 20:00:35 -0400

    update file upload button style

--- pages/profile.vue
+++ pages/profile.vue
@@ -132,16 +132,14 @@
                       class="mt-2 flex justify-center rounded-lg border border-dashed border-white/25 px-6 py-10">
                       <div class="text-center">
                         <PhotoIcon class="mx-auto h-12 w-12 text-gray-500" aria-hidden="true" />
-                        <div class="mt-4 flex text-sm leading-6 text-gray-400">
-                          <label for="file-upload"
-                            class="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-indigo-500">
-                            <span>Upload a file</span>
-                            <input id="file-upload" name="file-upload" type="file"
-                              class="sr-only" />
+                        <div class="my-2 flex items-center text-sm text-gray-400">
+                          <label for="file-upload" class="relative cursor-pointer rounded-md bg-green-800 font-bold text-black text-base px-4 py-2 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-indigo-500">
+                            <span class="">Upload a file</span>
+                            <input id="file-upload" name="file-upload" type="file" class="sr-only" />
                           </label>
-                          <p class="pl-1">or drag and drop</p>
+                          <p class="my-2 pl-2 text-base">or drag and drop</p>
                         </div>
-                        <p class="text-xs leading-5 text-gray-400">PNG, JPG, GIF up to 10MB</p>
+                        <p class="text-base leading-6 text-gray-400">WEBP, PNG, JPG, up to 2MB</p>
                       </div>
                     </div>
                   </div>
