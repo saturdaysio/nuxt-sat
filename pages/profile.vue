abad03111adbdfb820578390d3302500642027e5
pchae <20016424+pchae@users.noreply.github.com>
2023-08-01 11:08:03 -0400

    update label style

--- pages/profile.vue
+++ pages/profile.vue
@@ -117,17 +117,15 @@
 
 
                   <div class="col-span-full">
-                    <label for="photo" class="block text-sm font-medium leading-6 text-white">Photo</label>
+                    <label for="photo" class="block text-md font-bold leading-6 uppercase text-white">Photo</label>
                     <div class="mt-2 flex items-center gap-x-3">
-                      <UserCircleIcon class="block h-12 w-12 text-gray-500" aria-hidden="true" />
-                      <button type="button"
-                        class="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">Change</button>
+                      <UserCircleIcon class="block h-16 w-16 text-white" aria-hidden="true" />
+                      <button type="button" class="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">Change</button>
                     </div>
                   </div>
 
                   <div class="col-span-full">
-                    <label for="cover-photo" class="block text-sm font-medium leading-6 text-white">Cover
-                      photo</label>
+                    <label for="cover-photo" class="block text-md font-bold leading-6 uppercase text-white">Cover photo</label>
                     <div
                       class="mt-2 flex justify-center rounded-lg border border-dashed border-white/25 px-6 py-10">
                       <div class="text-center">
@@ -148,13 +146,11 @@
 
               <div v-if="user" class="max-w-5xl mx-auto border-b border-white/10 pb-12">
                 <h2 class="text-2xl font-bold leading-8 text-white">Personal Information</h2>
-                <p class="mt-1 text-base leading-6 text-gray-400">Use a permanent address where you can receive
-                  mail.</p>
+                <p class="mt-1 text-base leading-6 text-gray-400">Use a permanent address where you can receive mail.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                   <div class="sm:col-span-3">
-                    <label for="first-name" class="block text-sm font-medium leading-6 text-white">First
-                      name</label>
+                    <label for="first-name" class="block text-md font-bold leading-6 ppercase text-white">First name</label>
                     <div class="mt-2">
                       <input type="text" name="first-name" id="first-name" autocomplete="given-name"
                         class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
@@ -162,8 +158,7 @@
                   </div>
 
                   <div class="sm:col-span-3">
-                    <label for="last-name" class="block text-sm font-medium leading-6 text-white">Last
-                      name</label>
+                    <label for="last-name" class="block text-md font-medboldium leading-6 uppercase text-white">Last name</label>
                     <div class="mt-2">
                       <input type="text" name="last-name" id="last-name" autocomplete="family-name"
                         class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
@@ -171,8 +166,7 @@
                   </div>
 
                   <div class="sm:col-span-4">
-                    <label for="email" class="block text-sm font-medium leading-6 text-white">Email
-                      address</label>
+                    <label for="email" class="block text-md font-bold leading-6 uppercase text-white">Email address</label>
                     <div class="mt-2">
                       <input id="email" name="email" type="email" autocomplete="email" placeholder="{{ user.identities[0].identity_data.email }}"
                         class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
@@ -181,12 +175,12 @@
 
                   <div class="sm:col-span-3">
                     <label for="country"
-                      class="block text-sm font-medium leading-6 text-white">Country</label>
+                      class="block text-md font-bold leading-6 uppercase text-white">Country</label>
                     <div class="mt-2">
                       <select id="country" name="country" autocomplete="country-name"
                         class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black">
-                        <option>United States</option>
                         <option>Canada</option>
+                        <option>United States</option>
                         <option>Mexico</option>
                       </select>
                     </div>
@@ -194,7 +188,7 @@
 
                   <div class="col-span-full">
                     <label for="street-address"
-                      class="block text-sm font-medium leading-6 text-white">Street address</label>
+                      class="block text-md font-bold leading-6 uppercase text-white">Street address</label>
                     <div class="mt-2">
                       <input type="text" name="street-address" id="street-address"
                         autocomplete="street-address"
@@ -203,7 +197,7 @@
                   </div>
 
                   <div class="sm:col-span-2 sm:col-start-1">
-                    <label for="city" class="block text-sm font-medium leading-6 text-white">City</label>
+                    <label for="city" class="block text-md font-bold leading-6 uppercase text-white">City</label>
                     <div class="mt-2">
                       <input type="text" name="city" id="city" autocomplete="address-level2"
                         class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
@@ -211,8 +205,7 @@
                   </div>
 
                   <div class="sm:col-span-2">
-                    <label for="region" class="block text-sm font-medium leading-6 text-white">State /
-                      Province</label>
+                    <label for="region" class="block text-md font-bold leading-6 uppercase text-white">Province</label>
                     <div class="mt-2">
                       <input type="text" name="region" id="region" autocomplete="address-level1"
                         class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
@@ -220,8 +213,7 @@
                   </div>
 
                   <div class="sm:col-span-2">
-                    <label for="postal-code" class="block text-sm font-medium leading-6 text-white">ZIP /
-                      Postal code</label>
+                    <label for="postal-code" class="block text-md font-bold leading-6 uppercase text-white">Postal code</label>
                     <div class="mt-2">
                       <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code"
                         class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
@@ -236,7 +228,7 @@
 
                 <div class="mt-10 space-y-10">
                   <fieldset>
-                    <legend class="text-sm font-semibold leading-6 text-white">By Email</legend>
+                    <legend class="text-base font-bold leading-6 text-white">By Email</legend>
                     <div class="mt-6 space-y-6">
                       <div class="relative flex gap-x-3">
                         <div class="flex h-6 items-center">
@@ -244,7 +236,7 @@
                             class="h-4 w-4 rounded border-white/10 bg-white/5 text-indigo-600 focus:ring-indigo-600 focus:ring-offset-gray-900" />
                         </div>
                         <div class="text-sm leading-6">
-                          <label for="comments" class="font-medium text-white">Comments</label>
+                          <label for="comments" class="text-md font-bold text-white">Comments</label>
                           <p class="text-gray-400">Get notified when someones posts a comment on a
                             posting.</p>
                         </div>
@@ -255,7 +247,7 @@
                             class="h-4 w-4 rounded border-white/10 bg-white/5 text-indigo-600 focus:ring-indigo-600 focus:ring-offset-gray-900" />
                         </div>
                         <div class="text-sm leading-6">
-                          <label for="candidates" class="font-medium text-white">Candidates</label>
+                          <label for="candidates" class="text-md font-bold text-white">Candidates</label>
                           <p class="text-gray-400">Get notified when a candidate applies for a job.
                           </p>
                         </div>
@@ -266,7 +258,7 @@
                             class="h-4 w-4 rounded border-white/10 bg-white/5 text-indigo-600 focus:ring-indigo-600 focus:ring-offset-gray-900" />
                         </div>
                         <div class="text-sm leading-6">
-                          <label for="offers" class="font-medium text-white">Offers</label>
+                          <label for="offers" class="text-md font-bold text-white">Offers</label>
                           <p class="text-gray-400">Get notified when a candidate accepts or rejects an
                             offer.</p>
                         </div>
@@ -295,7 +287,7 @@
 <script setup lang="ts">
 
 	import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-	import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+	import { Bars3Icon, BellIcon, UserCircleIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 	const userProfile = {
 		name: 'Hello Saturday',
