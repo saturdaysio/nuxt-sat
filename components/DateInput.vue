106840a71bd98e56f032ca2a1e11f003d1b2fa2d
pchae <20016424+pchae@users.noreply.github.com>
2023-10-27 11:12:45 -0400

    update padding, icon and focus colours

--- components/DateInput.vue
+++ components/DateInput.vue
@@ -14,12 +14,12 @@ const {value} = defineProps<DateInputProps>()
         type="date"
         name="date"
         id="date"
-        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+        class="focus:ring-blue-500 focus:border-green-500 block w-full pl-3 pr-3 sm:text-sm border-gray-400 rounded-md"
         placeholder="mm/dd/yyyy"
         :value="value"
     />
     <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-      <svg class="h-5 w-5 text-gray-400" x-description="Heroicon name: solid/calendar" xmlns="http://www.w3.org/2000/svg"
+      <svg class="h-5 w-5 text-white" x-description="Heroicon name: solid/calendar" xmlns="http://www.w3.org/2000/svg"
            viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd"
               d="M16 4h1a1 1 0 011 1v11a1 1 0 01-1 1H3a1 1 0 01-1-1V5a1 1 0 011-1h1V2a2 2 0 012-2h2a2 2 0 012 2v1h4V2a2 2 0 012-2h2a2 2 0 012 2v2zM3 8h14v9H3V8zm5 1a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1H9a1 1 0 01-1-1V9z"
