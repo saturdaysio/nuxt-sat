79677a073fb85651af29ab80e1a2a35d18fccf6b
pchae <20016424+pchae@users.noreply.github.com>
2023-10-27 11:22:24 -0400

    replace svg with heroicon

--- components/DateInput.vue
+++ components/DateInput.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 
+import {CalendarIcon} from '@heroicons/vue/20/solid'
+
 interface DateInputProps {
   value: string
 }
@@ -19,16 +21,11 @@ const {value} = defineProps<DateInputProps>()
         :value="value"
     />
     <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-      <svg class="h-5 w-5 text-white" x-description="Heroicon name: solid/calendar" xmlns="http://www.w3.org/2000/svg"
-           viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd"
-              d="M16 4h1a1 1 0 011 1v11a1 1 0 01-1 1H3a1 1 0 01-1-1V5a1 1 0 011-1h1V2a2 2 0 012-2h2a2 2 0 012 2v1h4V2a2 2 0 012-2h2a2 2 0 012 2v2zM3 8h14v9H3V8zm5 1a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 01-1 1H9a1 1 0 01-1-1V9z"
-              clip-rule="evenodd"/>
-      </svg>
+      <CalendarIcon class="h-5 w-5 text-white hover:text-green-400" aria-hidden="true"/>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss">
 
+<style scoped lang="scss">
 </style>
