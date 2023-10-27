99cd789724a03ea4e026b3d48c11c84eb5b202bc
pchae <20016424+pchae@users.noreply.github.com>
2023-10-27 11:13:12 -0400

    add disabled text colour state

--- components/CustomInput.vue
+++ components/CustomInput.vue
@@ -9,7 +9,7 @@
           :type="inputType"
           :name="inputName"
           :id="label"
-          class="block w-full h-12 rounded-md border-0 bg-gray-800/40 pl-4 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset focus:ring-green-400 sm:text-sm sm:leading-6 disabled:bg-gray-900/40"
+          class="block w-full h-12 rounded-md border-0 bg-gray-800/40 pl-4 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset focus:ring-green-400 sm:text-sm sm:leading-6 disabled:bg-gray-900/40 disabled:text-gray-500"
           :placeholder="placeholder"
           v-model="inputComputed"
           :value="value"
