9820ec2dba94ced54d02a66e64eebf4a4d05b8f8
pchae <20016424+pchae@users.noreply.github.com>
2023-10-30 15:09:08 -0400

    remove work prerender route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -69,7 +69,6 @@ export default defineNuxtConfig({
   routeRules: {
     // Homepage pre-rendered at build time
     '/': {prerender: true},
-    '/work': {prerender: true},
     '/about': {prerender: true},
     '/terms-of-service': {prerender: true},
     '/privacy-policy': {prerender: true},
