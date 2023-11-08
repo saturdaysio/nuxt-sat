7e2fc96f1f82d30346aee85bb888faedd2da7864
pchae <20016424+pchae@users.noreply.github.com>
2023-11-08 00:14:47 -0500

    create new page for supabase 1.0

---
+++ pages/confirm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>Waiting for login..</div>
+</template>
+
+
+<script setup lang="ts">
+
+	useHead({
+		title: 'Saturdays.io - Confirmation',
+		meta: [
+			{ name: 'description', content: 'confirmation page' }
+		]
+	})
+
+	const user = useSupabaseUser()
+
+	watch(user, () => {
+	if (user.value) {
+		return navigateTo('/')
+	}
+	},{ immediate: true })
+
+
+	watchEffect(() => {
+		if (user.value) {
+			navigateTo('/dashboard', { replace: true })
+		}
+	})
+
+</script>
+
+
+<style lang="scss" scoped>
+</style>
