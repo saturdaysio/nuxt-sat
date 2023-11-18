784c6ff5b6000a1e19ca80d8d09404edd5c78482
pchae <20016424+pchae@users.noreply.github.com>
2023-11-17 22:31:18 -0500

    add linebreaks

--- components/CombinedInput.vue
+++ components/CombinedInput.vue
@@ -36,17 +36,20 @@
 
 </script>
 
+
 <template>
   <div class="flex flex-col">
     <label v-if="label" class="block text-md font-semibold leading-4 uppercase text-white">{{ label }}</label>
     <div class="mt-1 relative rounded-md shadow-sm flex flex-row">
-      <input v-for="field in fields" :key="field.name" type="text" :name="field.name" :id="field.name"
-             :placeholder="field.placeholder"
-             class="`block w-full h-12 first:rounded-l-md last:rounded-r-md border-0 bg-gray-800/40 pl-4 py-2 text-base leading-6 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset focus:ring-green-400 disabled:bg-gray-900/40`"
-             :value="fieldValues.find(f => f.name === field.name)?.value" @input="onTypeHandler"/>
+      <input
+        v-for="field in fields" :key="field.name" type="text" :name="field.name" :id="field.name"
+        :placeholder="field.placeholder"
+        class="`block w-full h-12 first:rounded-l-md last:rounded-r-md border-0 bg-gray-800/40 pl-4 py-2 text-base leading-6 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-1 focus:ring-inset focus:ring-green-400 disabled:bg-gray-900/40`"
+        :value="fieldValues.find(f => f.name === field.name)?.value" @input="onTypeHandler"/>
     </div>
   </div>
 </template>
+
 
 <style scoped lang="scss">
 </style>
