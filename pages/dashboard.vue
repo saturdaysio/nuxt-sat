938a49bed3ba1e9cd52014833151ad43726c0a33
pchae <20016424+pchae@users.noreply.github.com>
2023-11-17 19:33:07 -0500

    add disable pageTransition to definePageMeta

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,74 +1,9 @@
-<template>
-	<div class="min-h-full">
-    
-    <Nav2 />
-
-    <main class="py-24">
-      <div class="mx-auto max-w-full px-4">
-        <!-- Your content -->
-		<section>
-        <header>
-          <!-- Secondary navigation -->
-
-          <!-- Heading -->
-          <div class="flex flex-col items-start justify-between gap-x-8 gap-y-4 bg-gray-800/10 border border-white/20 px-4 py-8 sm:flex-row sm:items-center  lg:px-8 ">
-            <div>
-              <div class="flex items-center gap-x-2">
-                <div class="flex-none rounded-full bg-green-400 p-1 text-green-400">
-                  <div class="h-2 w-2 rounded-full bg-current" />
-                </div>
-                <h1 class="flex gap-x-2 text-base leading-4">
-                  <span class="font-bold text-white">supabase</span>
-                  <span class="text-gray-600">/</span>
-                  <span class="font-bold text-white">saturdays-microsite</span>
-                </h1>
-              </div>
-              <p class="mt-2 text-xs leading-6 text-gray-400">Deploys from GitHub via main branch</p>
-            </div>
-            <div class="order-first flex-none rounded-full bg-green-400/10 px-4 py-2 text-sm font-medium text-green-400 ring-1 ring-inset ring-gray-400/30 sm:order-none">Production</div>
-          </div>
-
-          <!-- Stats -->
-          <div class="grid grid-cols-1 bg-gray-700/10 sm:grid-cols-2 lg:grid-cols-4">
-            <div v-for="(stat, statIdx) in stats" :key="stat.name" :class="[statIdx % 2 === 1 ? 'sm:border-l' : statIdx === 2 ? 'lg:border-l' : '', 'border border-white/20 py-6 px-4 sm:px-6 lg:px-8']">
-              <p class="text-sm font-medium leading-6 text-gray-400">{{ stat.name }}</p>
-              <p class="mt-2 flex items-baseline gap-x-2">
-                <span class="text-4xl font-semibold tracking-tight text-white">{{ stat.value }}</span>
-                <span v-if="stat.unit" class="text-sm text-gray-400">{{ stat.unit }}</span>
-              </p>
-            </div>
-          </div>
-        </header>
-
-        <!-- Activity list -->
-		<div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-2 gap-y-2 pt-4">
-			<div v-for="item in quickLinks" :key="item.id" class="">
-				<div class="card rounded-sm border border-gray-200 hover:border-green-400 text-white hover:text-blue-400">
-					<NuxtLink :to="item.to">
-						<div class="image-thumb bg-gray-800/40">
-							<img :src="item.imageUrl" :alt="item.alt" class="object-fit" loading="lazy" :width="item.width" :height="item.height" />
-						</div>
-						<div class="data-bot bg-black p-4">
-							<div class="text-base sm:text-lg md:text-xl font-bold">
-								{{ item.name }}
-							</div>
-						</div>
-					</NuxtLink>
-				</div>
-			</div>
-		</div>
-
-	</section>
-      </div>
-    </main>
-  </div>
-</template>
-
-
 <script setup lang="ts">
 
 	definePageMeta({
-		middleware: ['auth']
+		middleware: ['auth'],
+		pageTransition: false,
+		layoutTransition: false
 	})
 
 	useHead({
@@ -188,6 +123,73 @@
 	]
 
 </script>
+
+
+<template>
+	<div class="min-h-full">
+    
+    <Nav2 />
+
+    <main class="py-24">
+      <div class="mx-auto max-w-full px-4">
+        <!-- Your content -->
+		<section>
+        <header>
+          <!-- Secondary navigation -->
+
+          <!-- Heading -->
+          <div class="flex flex-col items-start justify-between gap-x-8 gap-y-4 bg-gray-800/10 border border-white/20 px-4 py-8 sm:flex-row sm:items-center  lg:px-8 ">
+            <div>
+              <div class="flex items-center gap-x-2">
+                <div class="flex-none rounded-full bg-green-400 p-1 text-green-400">
+                  <div class="h-2 w-2 rounded-full bg-current" />
+                </div>
+                <h1 class="flex gap-x-2 text-base leading-4">
+                  <span class="font-bold text-white">supabase</span>
+                  <span class="text-gray-600">/</span>
+                  <span class="font-bold text-white">saturdays-microsite</span>
+                </h1>
+              </div>
+              <p class="mt-2 text-xs leading-6 text-gray-400">Deploys from GitHub via main branch</p>
+            </div>
+            <div class="order-first flex-none rounded-full bg-green-400/10 px-4 py-2 text-sm font-medium text-green-400 ring-1 ring-inset ring-gray-400/30 sm:order-none">Production</div>
+          </div>
+
+          <!-- Stats -->
+          <div class="grid grid-cols-1 bg-gray-700/10 sm:grid-cols-2 lg:grid-cols-4">
+            <div v-for="(stat, statIdx) in stats" :key="stat.name" :class="[statIdx % 2 === 1 ? 'sm:border-l' : statIdx === 2 ? 'lg:border-l' : '', 'border border-white/20 py-6 px-4 sm:px-6 lg:px-8']">
+              <p class="text-sm font-medium leading-6 text-gray-400">{{ stat.name }}</p>
+              <p class="mt-2 flex items-baseline gap-x-2">
+                <span class="text-4xl font-semibold tracking-tight text-white">{{ stat.value }}</span>
+                <span v-if="stat.unit" class="text-sm text-gray-400">{{ stat.unit }}</span>
+              </p>
+            </div>
+          </div>
+        </header>
+
+        <!-- Activity list -->
+		<div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-2 gap-y-2 pt-4">
+			<div v-for="item in quickLinks" :key="item.id" class="">
+				<div class="card rounded-sm border border-gray-200 hover:border-green-400 text-white hover:text-blue-400">
+					<NuxtLink :to="item.to">
+						<div class="image-thumb bg-gray-800/40">
+							<img :src="item.imageUrl" :alt="item.alt" class="object-fit" loading="lazy" :width="item.width" :height="item.height" />
+						</div>
+						<div class="data-bot bg-black p-4">
+							<div class="text-base sm:text-lg md:text-xl font-bold">
+								{{ item.name }}
+							</div>
+						</div>
+					</NuxtLink>
+				</div>
+			</div>
+		</div>
+
+	</section>
+      </div>
+    </main>
+  </div>
+</template>
 
 
 <style lang="scss">
