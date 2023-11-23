ba5c642fb960422f43bb688e66ffcff0ea41c141
pchae <20016424+pchae@users.noreply.github.com>
2023-11-22 19:19:13 -0500

    moved to store folder

---
+++ store/event.ts
@@ -0,0 +1,40 @@
+
+import {IAlgoliaSearchResult} from "~/utils/search/searchUtil";
+import {IEvent} from "~/utils/interfaces/Event";
+
+interface State {
+  data: IAlgoliaSearchResult<IEvent>,
+  query: string,
+}
+
+export const useEventStore = defineStore({
+  id: 'event',
+  state: (): State => {
+    return {
+      query: '',
+      data: {
+        hits: [],
+        nbHits: 0,
+        page: 0,
+        nbPages: 0,
+        hitsPerPage: 0,
+        exhaustiveNbHits: false,
+        query: '',
+        params: '',
+        processingTimeMS: 0,
+      }
+    }
+  },
+  getters: {
+    getResults: (state: State): IAlgoliaSearchResult<IEvent> => state.data,
+    getQuery: (state: State): string => state.query
+  },
+  actions: {
+    setResults(results: IAlgoliaSearchResult<IEvent>) {
+      this.data = results
+    },
+    setQuery(query: string) {
+      this.query = query
+    }
+  }
+})
