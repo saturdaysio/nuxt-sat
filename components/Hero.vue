90965254bcc4159a022d1483f88587f4fc926a1f
pchae <20016424+pchae@users.noreply.github.com>
2023-11-30 19:58:35 -0500

    add card array and refactor template

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,19 +1,92 @@
 <script setup lang="ts">
 
-	const props = defineProps({
-		text: String,
+	// Array of card images
+	const cards = [
+		{
+			id: '0',
+			src: "/card/cnn-02.png",
+	  		alt: "Canadian",
+	  		width: "750px",
+	  		height: "1334px"
+		},
+		{
+			id: '1',
+			src: "/card/cnn-featured.png",
+	  		alt: "USA Today",
+	  		width: "750px",
+	  		height: "1334px"
+		},
+		{
+			id: '2',
+			src: "/card/conor.png",
+	  		alt: "Reuters",
+	  		width: "416",
+	  		height: "566"
+		},
+		{
+			id: '3',
+			src: "/card/aperture-mode.webp",
+	  		alt: "Canadian",
+	  		width: "750px",
+	  		height: "912px"
+		},
+		{
+			id: '4',
+			src: "/card/max-siedentopf-selection-02.webp",
+	  		alt: "USA Today",
+	  		width: "750px",
+	  		height: "913px"
+		},
+		{
+			id: '5',
+			src: "/card/spc.png",
+	  		alt: "Reuters",
+	  		width: "414px",
+	  		height: "896px"
+		},
+		{
+			id: '6',
+			src: "/card/IMG_4604.png",
+	  		alt: "Reuters",
+	  		width: "414",
+	  		height: "896"
+		},
+		{
+			id: '7',
+			src: "/card/IMG_4608.png",
+	  		alt: "Reuters",
+	  		width: "414",
+	  		height: "896"
+		},
+	]
+
+	const initial = ref({
+		x: 2000,
+		opacity: 1,
 	})
 
 </script>
 
 
 <template>
-	<section class="w-full mx-auto px-4 py-16 bg-black">
-		<div class="container lg:max-w-6xl my-24 mx-auto">
-			<h1 class="text-4xl lg:text-6xl text-center font-bold text-white">
-				{{ text }}
-			</h1>
-			<!-- Add z-index and bg svg component -->
+	<section class="w-full mx-auto overflow-hidden">
+		<div class="mx-auto flex flex-row gap-x-12"
+			v-motion
+				:initial="initial"
+				:enter="{
+					x: -2000,
+					opacity: 1,
+					transition: {
+						duration: 24000,
+						ease: 'linear',
+						repeat: Infinity,
+						repeatType: 'loop',
+					}
+				}">
+
+			<div v-for="item in cards" :key="item.id" class="shrink-0">
+				<img :src="item.src" :alt="item.alt" :width="item.width" :height="item.height" class="object-none" />
+			</div>
 		</div>
 	</section>
 </template>
