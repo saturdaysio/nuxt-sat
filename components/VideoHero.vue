5287fd1e18a0582ee80fb9ff892e09d12a3d7f32
pchae <20016424+pchae@users.noreply.github.com>
2023-12-19 08:54:46 -0500

    set height 800px

--- components/VideoHero.vue
+++ components/VideoHero.vue
@@ -9,17 +9,17 @@
 
 <template>
 
-    <section id="video-hero" class="w-full max-h-screen mx-auto overflow-hidden">
-        <div class="relative w-full mx-auto z-0">
+    <section id="video-hero" class="w-full h-200 mx-auto overflow-hidden z-0">
+        <div class="relative w-full h-full mx-auto">
             <div id="hero-text-overlay" class="absolute w-full h-full mx-auto z-30 flex">
-                <div class="self-center mx-auto">
+                <div class="inline-flex self-center mx-auto">
                     <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight leading-tight text-center text-white">
                         {{ intro }}
                     </h1>
                 </div>
             </div>
 
-            <video id="hero-video" autoplay muted loop playsinline class=" h-full w-full" controlslist="nodownload noremoteplayback disablepictureinpicture disableremoteplayback">
+            <video id="hero-video" autoplay muted loop playsinline class="w-full h-screen object-cover" controlslist="nodownload noremoteplayback disablepictureinpicture disableremoteplayback">
                 <source src="@/assets/video/hero-video.mp4" type="video/mp4">
             </video>
 
