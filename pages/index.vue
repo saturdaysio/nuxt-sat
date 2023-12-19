38041c52a2f23683bad5e88a17103cbdcca47bf0
pchae <20016424+pchae@users.noreply.github.com>
2023-12-19 08:09:22 -0500

    add spacer div

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,10 @@
 
 		<Nav />
 
-		<Header intro="Saturdays is a Canadian digital studio based in Toronto" />
+		<div class="h-16">
+        </div>
 
-		<section class="px-4 sm:px-8 md:px-12 py-16 md:py-20 lg:py-24">
+		<section class="px-4 sm:px-8 md:px-12 py-16 md:py-20 lg:py-24 border-y border-gray-400/20">
 			<div class=" max-w-6xl mx-auto">
 				<h1 class="text-3xl md:text-4xl lg:text-5xl text-center font-bold lime-to-aqua">
 					We're a Data-driven digital studio that focuses on the Design, Development, and Strategy of digital products.
